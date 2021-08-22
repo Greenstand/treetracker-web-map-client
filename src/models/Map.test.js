3de05d69da1b3dd323b11c75896dc951454d7e7b
Mitchell Mayeda <mayedamitch@gmail.com>
2021-08-22 14:08:07 -0600

    test: fix map test

--- src/models/Map.test.js
+++ src/models/Map.test.js
@@ -6,25 +6,42 @@ jest.mock("./Requester");
 
 describe("Map", () => {
 
-  it("getInitialView", async () => {
+  beforeEach(() => {
+    Requester.mockClear();
+  })
+
+  it("loadInitialView", async () => {
     const request = jest.fn(() => response);
     Requester.mockImplementation(() => ({
       request,
     }));
+    const view = {};
     const map = new Map({
       userid: "1",
       width: 1440,
       height: 510,
+      moreEffect: false,
+      filters: {
+        wallet: "mayeda",
+      },
+      map: {
+        setView: jest.fn((center, zoomLevel, animate) => {
+          view.center = center;
+          view.zoomLevel = zoomLevel;
+          view.animate = animate;
+        })
+      }
     });
+    expect(Requester).toHaveBeenCalledTimes(1);
 
-    const view = await map.getInitialView();
+    await map.loadInitialView();
     expect(request).toHaveBeenCalled();
     expect(view).toMatchObject({
       center: {
         lat: 18.788082619896404,
         lng: -61.031384143776386,
       },
-      zoomLevel: 3
+      zoomLevel: 3,
     });
   });
 
