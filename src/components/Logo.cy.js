fc890054fc225ca8e5bb68d40cab274ad80ff5da
Mitchell Mayeda <mayedamitch@gmail.com>
2021-09-18 12:57:21 -0600

    refactor: fix cypress test

--- src/components/Logo.cy.js
+++ src/components/Logo.cy.js
@@ -1,22 +1,15 @@
-import React from "react"
 import { mount } from "cypress-react-unit-test";
+import React from "react"
+
+import logo from "../images/greenstand_logo_full.png"
 import Logo from "./Logo"
 
 describe("Logo", () => {
   it("it shows logo", () => {
-    const testSrc = "test-source";
-    mount(<Logo logoSrc={testSrc} logoLoaded={true}/>);
-    cy.get("img")
-      .should("be.visible")
-      .should("have.attr", "src", testSrc);
-    cy.get("a").should("not.exist");
-  });
-  it("links to greenstand when greenstand logo is used", () => {
-    const testSrc = require("../images/logo_floating_map.svg");
-    mount(<Logo logoSrc={testSrc} logoLoaded={true}/>)
+    mount(<Logo/>)
     cy.get("a > img")
       .should("be.visible")
-      .should("have.attr", "src", testSrc);
+      .should("have.attr", "src", logo);
     cy.get("a")
       .should("exist")
       .should("have.attr", "href", "https://greenstand.org/")
