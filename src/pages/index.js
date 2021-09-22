8dc98b290cf70eb8a0630f1e45943e036de5dcfe
Charlie Edwards <charles.edwards1811@gmail.com>
2021-09-22 18:02:52 +1000

    fix: resolving conflict

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 
-const DynamicApp = dynamic(() => import('../MapComponent'), { ssr: false });
+const DynamicApp = dynamic(() => import('../App'), { ssr: false });
 
 export default function Home() {
   return <div>Welcome<DynamicApp /></div>;
