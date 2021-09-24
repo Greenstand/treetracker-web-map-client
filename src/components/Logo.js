ce20439f2fbeb196023ce641fc40d2bfaeb837c4
Mitchell Mayeda <mayedamitch@gmail.com>
2021-09-23 19:59:39 -0600

    feat: map logo has a frame and also displays 'powered by greenstand'

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,45 +1,72 @@
-import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
+import React from "react";
+
+import greenstandLogo from "../images/greenstand_logo_full.png"
+
+const logoWidth = 150;
+const logoWidthSm = 100;
 
 const useStyles = makeStyles(theme => ({
   logo: {
     userSelect: "none",
     position: "absolute",
     right: 60,
-    bottom: 20,
+    bottom: 22,
     opacity: 0,
+    padding: 10,
+    width: logoWidth,
+    "background-color": "white",
     transform: "translate(0, 40px)",
     transition: "all 1s",
+    textAlign: "center",
+    fontSize: 10,
+    fontFamily: "Roboto, Arial, sans-serif",
+    borderRadius: 25,
     "& img": {
-      maxWidth: 250,
+      width: logoWidth - 70,
+    },
+    "& #custom-logo": {
+      width: logoWidth,
     },
     [theme.breakpoints.down("sm")]: {
+      width: logoWidthSm,
       pointerEvents: "none",
       transform: "translate(0, -20px)",
       right: 10,
       top: 10,
+      fontSize: 8,
+      bottom: "auto",
       "& img": {
-        width: "45vw",
+        width: logoWidthSm - 50,
+      },
+      "& #custom-logo": {
+        width: logoWidthSm,
       },
     },
   },
   logoLoaded: {
     transform: "translate(0, 0)",
-    opacity: 1,
+    opacity: 0.87,
   },
 }));
 
 function Logo(props){
   const classes = useStyles();
-  const isGreenstandLogo = props.logoSrc === require("../images/logo_floating_map.svg")
+
   return (
     <div className={`${classes.logo} ${props.logoLoaded?classes.logoLoaded:""}`}>
-      {isGreenstandLogo
-        ? <a href="https://greenstand.org/" target="_blank" rel="noopener noreferrer">
-            <img alt="logo" src={props.logoSrc} />
-          </a>
-        : <img alt="logo" src={props.logoSrc} />
+      {props.logoSrc &&
+        <div>
+          <img id="custom-logo" src={props.logoSrc}/>
+          <br/>
+        </div>
       }
+      <span>
+        Powered by{" "}
+        <a href="https://greenstand.org/" target="_blank" rel="noopener noreferrer">
+          <img alt="logo" src={greenstandLogo} />
+        </a>
+      </span>
     </div>
   );
 }
