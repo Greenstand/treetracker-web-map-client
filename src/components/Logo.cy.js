7d49b403627c52e06ea75152af90df99b5cabfc9
Mitchell Mayeda <mayedamitch@gmail.com>
2021-09-23 20:07:03 -0600

    test: fix cypress test

--- src/components/Logo.cy.js
+++ src/components/Logo.cy.js
@@ -3,16 +3,8 @@ import { mount } from "cypress-react-unit-test";
 import Logo from "./Logo"
 
 describe("Logo", () => {
-  it("it shows logo", () => {
-    const testSrc = "test-source";
-    mount(<Logo logoSrc={testSrc} logoLoaded={true}/>);
-    cy.get("img")
-      .should("be.visible")
-      .should("have.attr", "src", testSrc);
-    cy.get("a").should("not.exist");
-  });
   it("links to greenstand when greenstand logo is used", () => {
-    const testSrc = require("../images/logo_floating_map.svg");
+    const testSrc = require("../images/greenstand_logo_full.png");
     mount(<Logo logoSrc={testSrc} logoLoaded={true}/>)
     cy.get("a > img")
       .should("be.visible")
