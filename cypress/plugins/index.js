87a2b2c50f821e21e7048833dbbcbfb346df6288
Charlie Edwards <charles.edwards1811@gmail.com>
2021-09-27 18:14:51 +1000

    fix: #158 flash glitch

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -8,6 +8,3 @@ module.exports = (on, config) => {
 
         return config
 }
-~                                                                                                                
-~                                                                                                                
-~              
