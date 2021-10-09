478ffd0f84dd3a00633c7b67df0f39c93b736215
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-10-08 18:31:04 -0700

    feat: next/Link wrapper component

---
+++ src/components/Link.js
@@ -0,0 +1,63 @@
+import MuiLink from '@material-ui/core/Link';
+import clsx from 'clsx';
+import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+
+const NextComposed = React.forwardRef(function NextComposed(props, ref) {
+  const { as, href, ...other } = props;
+
+  return (
+    <NextLink href={href} as={as}>
+      <a ref={ref} {...other} />
+    </NextLink>
+  );
+});
+
+// A styled version of the Next.js Link component:
+// https://nextjs.org/docs/#with-link
+function LinkComponent(props) {
+  const {
+    href,
+    activeClassName = 'active',
+    className: classNameProps,
+    innerRef,
+    naked,
+    ...other
+  } = props;
+
+  const router = useRouter();
+  const pathname = typeof href === 'string' ? href : href.pathname;
+
+  const className = clsx(classNameProps, {
+    [activeClassName]: router.pathname === pathname && activeClassName,
+  });
+
+  if (naked) {
+    return (
+      <NextComposed
+        className={className}
+        ref={innerRef}
+        href={href}
+        {...other}
+      />
+    );
+  }
+
+  return (
+    <MuiLink
+      component={NextComposed}
+      className={className}
+      ref={innerRef}
+      href={href}
+      {...other}
+    />
+  );
+}
+
+const Link = React.forwardRef(
+  (props, ref) => <LinkComponent {...props} innerRef={ref} />,
+  { displayName: 'Link' },
+);
+
+export default Link;
