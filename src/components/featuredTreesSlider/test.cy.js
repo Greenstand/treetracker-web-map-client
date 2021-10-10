5152e30b8bfb39503aa274cca05885eb16f6c185
Yassin Hussein <4assin@gmail.com>
2021-10-10 10:05:00 +0000

    feat: add featured trees component

---
+++ src/components/featuredTreesSlider/test.cy.js
@@ -0,0 +1,8 @@
+import { mount } from '@cypress/react';
+import FeaturedTreesSlider from './index'
+
+describe('Featured Trees Slider', () => {
+  it('it shows featured trees', () => {
+    mount(<FeaturedTreesSlider />);
+  });
+});
--- src/pages/top.js
+++ src/pages/top.js
@@ -1,17 +1,19 @@
 import Link from 'next/link';
+import FeaturedTreesSlider from '../components/featuredTreesSlider'
 
 export default function Top() {
   return (
     <div>
       <h1>top page</h1>
-      <h2>featured trees</h2>
+      <h2>featured trees</h2> 
+      <FeaturedTreesSlider />
       <ul>
-        <li>
+        {/* <li>
           <Link href="/trees/933042">tree 933042</Link>
         </li>
         <li>
           <Link href="/trees/932946">tree 932946</Link>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
