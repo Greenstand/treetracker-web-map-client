335906a607024675e6a103818c5fd7dcc5a7d219
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-10-22 20:03:30 -0700

    fix: home page styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
@@ -8,66 +7,68 @@ import React from 'react';
 import backgroundImage from '../images/bg.png';
 import Link from './Link';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   pageContainer: {
     background: `center / cover no-repeat url(${backgroundImage})`,
-    display: 'flex',
     height: '100%',
-    alignItems: 'center',
   },
   contentContainer: {
-    color: 'white',
     width: '75%',
     margin: '0 auto',
+    color: 'white',
+    height: '100%',
+    lineHeight: '140%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    '&>*': {
+      marginBottom: theme.spacing(5),
+    },
   },
-  welcome: {
-    fontFamily: 'Montserrat !important',
-    fontSize: '20px !important',
-    fontStyle: 'normal !important',
-    fontWeight: '700 !important',
-    lineHeight: '26px !important',
-    letterSpacing: '0em !important',
-    textAlign: 'left !important',
+  buttonsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
   },
-  slogan: {
-    fontFamily: 'Montserrat !important',
-    fontSize: '40px !important',
-    fontStyle: 'normal !important',
-    fontWeight: '700 !important',
-    lineHeight: '63px !important',
-    letterSpacing: '0em !important',
-    textAlign: 'left !important',
-  },
+  welcome: {},
+  slogan: {},
   button: {
-    background: '#86C232',
+    textTransform: 'none',
+    marginRight: 20,
+  },
+  findATreeButton: {
+    textTransform: 'none',
+    background: theme.palette.success.main,
   },
 }));
 
-function Home() {
+export default function Home() {
   const classes = useStyles();
+
+  const Buttons = () => (
+    <Box className={classes.buttonsContainer}>
+      <Button variant="outlined" color="primary" className={classes.button}>
+        Learn more
+      </Button>
+      <Link href="/top">
+        <Button variant="contained" className={classes.findATreeButton}>
+          Let&apos;s Find a Tree
+        </Button>
+      </Link>
+    </Box>
+  );
+
   return (
-    <Grid className={classes.pageContainer}>
-      <Grid container className={classes.contentContainer}>
-        <Typography variant="h4" className={classes.welcome}>
+    <Box className={classes.pageContainer}>
+      <Box className={classes.contentContainer}>
+        <Typography variant="h3" className={classes.welcome}>
           Welcome to TreeTracker
         </Typography>
-        <Typography variant="h2" className={classes.slogan}>
+        <Typography variant="h1" className={classes.slogan}>
           Come explore the global reforestation effort.
         </Typography>
-        <Grid container>
-          <Button variant="outlined" color="primary">
-            Learn more
-          </Button>
-          <Box sx={{ width: 20 }} component="span" />
-          <Link href="/top" passHref>
-            <Button variant="contained" className={classes.button}>
-              Let&apos;s Find a Tree
-            </Button>
-          </Link>
-        </Grid>
-      </Grid>
-    </Grid>
+        <Buttons />
+      </Box>
+    </Box>
   );
 }
-
-export default Home;
