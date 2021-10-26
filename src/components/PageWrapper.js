5cf0ebc61adb7e5439f3a8f40189ed4f068d74f5
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-10-25 17:16:06 -0700

    fix: layout fixes and improvements

--- src/components/PageWrapper.js
+++ src/components/PageWrapper.js
@@ -1,25 +1,40 @@
 import { Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 
 import BackButton from './BackButton';
 import SearchButton from './SearchButton';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   pageContainer: {
-    background: 'white',
-    padding: '12px 20px',
+    margin: '12px 24px',
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1,
+  },
+  top: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  main: {
+    width: '100%',
+    flexGrow: 1,
   },
 }));
 
-export default function PageWrapper({ children }) {
+export default function PageWrapper({ children, className, ...otherProps }) {
   const classes = useStyles();
   return (
     <Box className={classes.pageContainer}>
-      <Box display="flex" justifyContent="spaceBetween">
+      <Box className={classes.top}>
         <BackButton />
         <SearchButton />
       </Box>
-      {children}
+      <Box className={clsx(classes.main, className)} {...otherProps}>
+        {children}
+      </Box>
     </Box>
   );
 }
