b2cf156d53a7a0ca2817a85fa4c1ebf3d9e157da
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-10-26 17:12:44 -0700

    fix: fix BackButton styles

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -1,21 +1,25 @@
-import { Button } from '@material-ui/core';
+import { ButtonBase } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   backButton: {
     textTransform: 'none',
+    fontSize: 16,
+    lineHeight: '16px',
+    color: '#6B6E70',
+  },
+  icon: {
+    marginRight: -4,
   },
 }));
 
 export default function BackButton() {
   const classes = useStyles();
   return (
-    <Button
-      className={classes.backButton}
-      color="textPrimary"
-      startIcon={<ArrowBackIosIcon />}
-    >
+    <ButtonBase className={classes.backButton} color="textPrimary">
+      <ArrowBackIosIcon fontSize="inherit" className={classes.icon} />
       Back
-    </Button>
+    </ButtonBase>
   );
 }
