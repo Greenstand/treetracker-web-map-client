8878cf68450ade437d1b16bde6ff46fe6cc0005e
deanchen <antlet2004@gmail.com>
2021-11-03 16:15:22 +0800

    chore: top integration passed

--- cypress/tests/integration/top.cy.js
+++ cypress/tests/integration/top.cy.js
@@ -1,6 +1,23 @@
+import tree186734 from '../../fixtures/tree186734.json';
+
 describe('top', () => {
+  beforeEach(() => {
+    cy.task('clearNock');
+  });
+
   it('top page', () => {
-    cy.visit('/');
-    cy.contains('top page');
+    cy.task('nock', {
+      hostname: 'http://127.0.0.1:4010/mock',
+      method: 'GET',
+      path: '/trees/featured',
+      statusCode: 200,
+      body: {
+        trees: [{ ...tree186734 }],
+      },
+    });
+    cy.visit('/top');
+    cy.contains('Featured Trees');
+    cy.contains('Check out the global leaders in the tree planting effort');
+    cy.contains(`${tree186734.id  }`);
   });
 });
--- src/pages/top.js
+++ src/pages/top.js
@@ -1,22 +1,36 @@
+import log from 'loglevel';
+
 import Link from '../components/Link';
 
-export default function Top() {
+export default function Top({ trees }) {
   return (
     <div>
       <h1>top page</h1>
-      <h2>featured trees</h2>
+      <h2>Featured Trees</h2>
       <ul>
-        <li>
-          <Link href="/trees/933042" color="inherit">
-            tree 933042
-          </Link>
-        </li>
-        <li>
-          <Link href="/trees/932946" color="inherit">
-            tree 932946
-          </Link>
-        </li>
+        {trees.map((tree) => (
+          <li key={tree.id}>
+            <Link href="/trees/{tree.id}">tree {tree.id}</Link>
+          </li>
+        ))}
       </ul>
+      <h2>Check out the global leaders in the tree planting effort</h2>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const url = `${process.env.NEXT_PUBLIC_API_NEW}/trees/featured`;
+  log.warn('url:', url);
+
+  const res = await fetch(url);
+  const data = await res.json();
+  log.warn('response:', data);
+
+  return {
+    props: {
+      trees: data.trees,
+    },
+  };
+  return { props: {} };
 }
