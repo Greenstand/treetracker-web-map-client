a9b069e87ebeec1e45d980a54c98bd8bfc11a7a6
akash5100 <shivamvermashs@gmail.com>
2021-11-03 14:11:41 +0530

    added link to Navbar logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -48,7 +48,7 @@ function Navbar() {
   const classes = useStyles();
   return (
     <AppBar className={classes.navContainer} color="primary" position="static">
-      <Image src={logo} width={180} height={30} />
+      <a href="https://greenstand.org/"><Image src={logo} width={180} height={30} /></a>
       <Toolbar variant="dense" className={classes.toolbar}>
         <Link href="/">
           <Button className={classes.buttonStyle} variant="text">
