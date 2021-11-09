64e9ab14988b103672b60922205ceb5d175f255e
Lizzie Chai <lizzie.chai@gmail.com>
2021-11-09 10:59:46 -0600

    fix: #253 on initial page load clicking on link rerenders page

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -25,23 +25,3 @@ export default class MyDocument extends Document {
     );
   }
 }
-
-MyDocument.getInitialProps = async (ctx) => {
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-    });
-
-  const initialProps = await Document.getInitialProps(ctx);
-
-  return {
-    ...initialProps,
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      sheets.getStyleElement(),
-    ],
-  };
-};
