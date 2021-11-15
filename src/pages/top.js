52c02d9a6d2afc5d610a78765fa0472254f94cc7
deanchen <antlet2004@gmail.com>
2021-11-15 16:18:21 +0800

    feat: contry leader; jump to country;

--- src/pages/top.js
+++ src/pages/top.js
@@ -2,8 +2,26 @@ import Typography from '@material-ui/core/Typography';
 import log from 'loglevel';
 
 import Link from '../components/Link';
+import { useMapContext } from '../mapContext';
+import * as utils from '../models/utils';
 
 export default function Top({ trees, countries }) {
+  // use map context to get the map
+  const mapContext = useMapContext();
+
+  async function handleCountryClick(countryId) {
+    log.debug('handleCountryClick', countryId);
+    // use utils.requestAPI to request API
+    const country = await utils.requestAPI(`/countries/${countryId}`);
+    // print country
+    log.debug('country', country);
+
+    const { lat, lon } = country.centroid;
+
+    const { map } = mapContext;
+    map.flyTo(lat, lon, 6);
+  }
+
   return (
     <div>
       <h1>top page</h1>
@@ -19,7 +37,11 @@ export default function Top({ trees, countries }) {
       </ul>
       <h2>Check out the global leaders in the tree planting effort</h2>
       {countries.map((country) => (
-        <li key={country.id}>{country.name}</li>
+        <li key={country.id}>
+          <a onClick={() => handleCountryClick(country.id)}>
+            {country.name} planted: {country.planted}
+          </a>
+        </li>
       ))}
     </div>
   );
