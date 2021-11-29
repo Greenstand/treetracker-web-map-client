6da0fd7ff78b56a8f5d6fa49ee6af4c5bcd7af0e
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-11-28 23:18:54 -0800

    chore: configure tss-react

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
+// eslint-disable-next-line import/extensions
 import '../style.css';
 
+import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
 import { ThemeProvider } from '@mui/material/styles';
 import React from 'react';
@@ -8,13 +10,17 @@ import Layout from '../components/Layout';
 import { MapContextProvider } from '../mapContext';
 import appTheme from '../theme';
 
-function TreetrackerApp({
-  Component,
-  pageProps,
-  emotionCache = clientSideEmotionCache,
-}) {
+let muiCache;
+
+export const createMuiCache = () =>
+  (muiCache = createCache({
+    key: 'mui',
+    prepend: true,
+  }));
+
+function TreetrackerApp({ Component, pageProps }) {
   return (
-    <CacheProvider value={emotionCache}>
+    <CacheProvider value={muiCache ?? createMuiCache()}>
       <ThemeProvider theme={appTheme}>
         <MapContextProvider>
           <Layout>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,10 @@
-import createEmotionServer from '@emotion/server/create-instance';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
+import { withEmotionCache } from 'tss-react/nextJs';
 
-import createEmotionCache from '../models/createEmotionCache';
+import { createMuiCache } from './_app';
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
@@ -28,36 +28,12 @@ export default class MyDocument extends Document {
   }
 }
 
-MyDocument.getInitialProps = async (ctx) => {
-  const originalRenderPage = ctx.renderPage;
-
-  const cache = createEmotionCache();
-  const { extractCriticalToChunks } = createEmotionServer(cache);
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      // eslint-disable-next-line react/display-name
-      enhanceApp: (App) => (props) => <App emotionCache={cache} {...props} />,
-    });
-
-  const initialProps = await Document.getInitialProps(ctx);
-
-  // This is important. It prevents emotion to render invalid HTML.
-  // See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
-  const emotionStyles = extractCriticalToChunks(initialProps.html);
-  const emotionStyleTags = emotionStyles.styles.map((style) => (
-    <style
-      data-emotion={`${style.key} ${style.ids.join(' ')}`}
-      key={style.key}
-      dangerouslySetInnerHTML={{ __html: style.css }}
-    />
-  ));
-
-  return {
-    ...initialProps,
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      ...emotionStyleTags,
-    ],
-  };
-};
+export default withEmotionCache({
+  /** If you have a custom document pass it instead */
+  Document: MyDocument,
+  /**
+   * Every emotion cache used in the app should be provided.
+   * Caches for MUI should use "prepend": true.
+   * */
+  getCaches: () => [createMuiCache()],
+});
