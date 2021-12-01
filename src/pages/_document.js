5eebed072421f45dd0764ca13fc8527bc7a3e149
Jay Clark <jay@jayeclark.dev>
2021-12-01 10:34:41 -0400

    fix: google font imports

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
           <link
