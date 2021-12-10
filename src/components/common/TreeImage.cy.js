bc9635b07b2377134cb5c0118baff044ed1f3f26
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-12-10 08:22:29 -0400

    feat: add tree image tests

---
+++ src/components/common/TreeImage.cy.js
@@ -0,0 +1,55 @@
+import { mountWithTheme as mount } from '../../models/test-utils';
+import React from 'react';
+import data from '../../../cypress/fixtures/tree186734.json';
+
+import TreeImage from './TreeImage';
+
+describe('TreeImage', () => {
+  it('The maximum icon link points to the right location', () => {
+    cy.viewport(1440, 700);
+    mount(
+      <TreeImage
+        imageUrl={data.image_url}
+        timeCreated={data.time_created}
+        likes={20}
+      />,
+    );
+    cy.get('.tss-rvai9x-container')
+      .trigger('mouseover')
+      .get('a')
+      .invoke('attr', 'href')
+      .should('equal', `${data.image_url}`);
+  });
+
+  it('Image renders properly', () => {
+    cy.viewport(1440, 700);
+    mount(
+      <TreeImage
+        imageUrl={data.image_url}
+        timeCreated={data.time_created}
+        likes={20}
+      />,
+    );
+    cy.get('[alt="tree image"]')
+      .should('be.visible')
+      .and(($img) => {
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+      });
+  });
+
+  it('The date renders properly ', () => {
+    cy.viewport(1440, 700);
+    mount(
+      <TreeImage
+        imageUrl={data.image_url}
+        timeCreated={data.time_created}
+        likes={20}
+      />,
+    );
+
+    cy.get('.tss-rvai9x-container')
+      .trigger('mouseover')
+      .get('.tss-1shtwbt-year')
+      .contains('1/9/2020');
+  });
+});
