bc3cbe624e6b5458346c72b612c2d6253b27e251
VWRoli <fustroli@gmail.com>
2021-12-12 14:38:44 +0100

    fix: issue #359 makeStyles import error

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -1,29 +1,13 @@
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import { ButtonBase, Typography } from '@mui/material';
+import ButtonBase from '@mui/material/ButtonBase';
+import Typography from '@mui/material/Typography';
 import React from 'react';
 
-const useStyles = makeStyles()(() => ({
-  backButton: {
-    textTransform: 'none',
-    fontSize: 16,
-    lineHeight: '16px',
-    color: '#6B6E70',
-  },
-  icon: {
-    marginRight: -4,
-  },
-}));
-
 export default function BackButton({ onBackHandler }) {
-  const { classes } = useStyles();
   return (
-    <ButtonBase
-      onClick={onBackHandler}
-      className={classes.backButton}
-      color="textPrimary"
-    >
-      <ArrowBackIosIcon fontSize="inherit" className={classes.icon} />
-      Back
+    <ButtonBase onClick={onBackHandler} sx={{ color: 'textLight.main' }}>
+      <ArrowBackIosIcon fontSize="inherit" />
+      <Typography>Back</Typography>
     </ButtonBase>
   );
 }
