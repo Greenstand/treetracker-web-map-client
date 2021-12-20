fc465e20727d84505a314d3b529fd195e86c2c16
VWRoli <fustroli@gmail.com>
2021-12-20 15:03:25 +0100

    fix: issue #366 org image stretched

--- src/pages/organizations/[organizationid].js
+++ src/pages/organizations/[organizationid].js
@@ -1,19 +1,19 @@
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
-import { Grid, Avatar, Box, Divider, Typography } from '@mui/material';
+import ParkOutlinedIcon from '@mui/icons-material/ParkOutlined';
+import { Avatar, Box, Divider, Grid, Typography } from '@mui/material';
+import TreeSpeciesCard from 'components/TreeSpeciesCard';
 import log from 'loglevel';
 import { makeStyles } from 'models/makeStyles';
 import React from 'react';
-import ParkOutlinedIcon from '@mui/icons-material/ParkOutlined';
-import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
 
+import CustomCard from '../../components/common/CustomCard';
 import PageWrapper from '../../components/PageWrapper';
 import VerifiedBadge from '../../components/VerifiedBadge';
 // import placeholder from '../../images/organizationsPlaceholder.png';
 import { useMapContext } from '../../mapContext';
 import * as utils from '../../models/utils';
-import CustomCard from '../../components/common/CustomCard';
-import TreeSpeciesCard from 'components/TreeSpeciesCard';
 
 const useStyles = makeStyles()((theme) => ({
   info: {
@@ -37,7 +37,9 @@ const useStyles = makeStyles()((theme) => ({
   imgContainer: {
     borderRadius: '16px',
     position: 'relative',
-    height: 332,
+    img: {
+      borderRadius: '16px',
+    },
     marginBottom: theme.spacing(4),
     '& img:nth-of-type(1)': {
       width: '100%',
@@ -123,6 +125,7 @@ export default function Organization({ organization }) {
         {area}, {country}
       </Box>
       <Divider className={classes.divider} />
+
       <Box className={classes.imgContainer}>
         <img src={photo_url} />
         <img src={logo_url} />
@@ -134,7 +137,7 @@ export default function Organization({ organization }) {
             icon={<ParkOutlinedIcon />}
             title="Trees Planted"
             text={organization?.featuredTrees?.total}
-            disabled={isPlanterTab ? true : false}
+            disabled={!!isPlanterTab}
           />
         </Grid>
         <Grid item>
@@ -146,13 +149,13 @@ export default function Organization({ organization }) {
             icon={<GroupsOutlinedIcon />}
             title="Associated Organizations"
             text={organization?.associatedPlanters?.total}
-            disabled={isPlanterTab ? false : true}
+            disabled={!isPlanterTab}
           />
         </Grid>
       </Grid>
       {!isPlanterTab && (
         <div>
-          {/*TODO replace with the world map component */}
+          {/* TODO replace with the world map component */}
           <h5>The world map</h5>
           <h5>
             {organization?.associatedPlanters?.total} tree planted in continent{' '}
