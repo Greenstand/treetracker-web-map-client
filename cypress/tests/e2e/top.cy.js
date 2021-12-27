019d0e7878b958d792662a5c1131f109c5ce5be6
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-12-27 01:51:32 -0800

    fix: top.cy.js e2e test

--- cypress/tests/e2e/top.cy.js
+++ cypress/tests/e2e/top.cy.js
@@ -14,9 +14,12 @@ describe('Top Page', () => {
     cy.visit('/top');
     cy.contains('Filters');
     // cypress to find the date type of input, choose the first one, and input the date
-    cy.get('input[type=date]').first().type('2008-01-01');
+    cy.contains('label', 'Start Date')
+      .parent()
+      .find('input')
+      .type('2008-01-01');
     // find the second date type of input, choose the second one, and input the date
-    cy.get('input[type=date]').eq(1).type('2020-01-02');
+    cy.contains('label', 'End Date').parent().find('input').type('2020-01-02');
     cy.contains(/submit/i).click();
   });
 });
