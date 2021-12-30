0588662eda352f09e812a3e19a9ab20cdc30d91d
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-12-29 22:09:05 -0800

    chore: add caching to lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,8 +3,8 @@ module.exports = {
   '*': ['prettier --ignore-unknown --write'],
 
   // lint javascript after formatting
-  '*.{js,jsx}': ['eslint --fix'],
+  '*.{js,jsx}': ['eslint --fix --cache'],
 
   // lint entire project if eslint settings changed, do not pass file name arguments
-  '.eslint*': () => 'eslint .',
+  '.eslint*': () => 'eslint . --cache',
 };
