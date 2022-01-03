5d9e6ee25d90a990a402017d08d2461a3b2ecd64
Stanley Pierre-Louis <stanleypl75@gmail.com>
2022-01-03 16:30:27 -0400

    style: update styles

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,133 +1,105 @@
-import React from 'react';
-import withStyles from '@mui/styles/withStyles';
-import Box from '@mui/material/Box';
-import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
+import { Global } from '@emotion/react';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Typography from '@mui/material/Typography';
+import withStyles from '@mui/styles/withStyles';
 import log from 'loglevel';
-
-const style = (theme) => ({
-  map: {
-    width: '100vw',
-    height: '100vh',
-  },
-  paper: {
-    background: 'transparent',
-    overflow: 'visible',
-  },
-  rounded: {
-    borderRadius: 20,
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0,
-  },
-  drawer: {
-    height: '80vh',
-    width: '100%',
-    overflow: 'auto',
-    '&::-webkit-scrollbar': {
-      display: 'none',
-    },
-  },
-  box: {
-    justifyContent: 'center',
-  },
-  bottomPaper: {
-    width: '100%',
-    position: 'fixed',
-    bottom: 0,
-    boxShadow: '0 -1px 2px rgb(0 0 0 / 30%)',
-    zIndex: 1000,
-    borderRadius: '8px 8px 0 0 ',
-  },
-  bottomBox: {
-    height: theme.spacing(6),
-  },
-  bottomArrow: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: theme.spacing(1),
-  },
-  bottomContent: {
-    flexGrow: 1,
-  },
-  bottomItem: {
-    marginLeft: theme.spacing(1),
-  },
-  avatar: {
-    width: theme.spacing(4),
-    height: theme.spacing(4),
-  },
-  box1: {
-    height: '100%',
-    alignItems: 'center',
-    width: '100%',
-  },
-  handle: {
-    backgroundColor: '#DADCE0',
-    borderRadius: 50,
-    height: 4,
-    margin: '0 auto',
-    width: 24,
-    marginTop: 10,
-  },
-});
+import React, { useEffect, useRef, useState } from 'react';
 
 function Drawer(props) {
-  const { classes, children } = props;
-  const [open, setOpen] = React.useState(true);
-
-  // console.log("Tokens List", tokens)
+  const { children } = props;
+  const [open, setOpen] = useState(true);
 
   function handleClickBottom() {
     setOpen(true);
   }
-
   return (
     <>
+      <Global
+        styles={{
+          '.MuiDrawer-root > .MuiPaper-root': {
+            height: `90vh`,
+            overflow: 'visible',
+            // position: 'absolute',
+          },
+        }}
+      />
       <SwipeableDrawer
         anchor={'bottom'}
         open={open}
-        classes={{ paper: classes.paper }}
         onClose={() => setOpen(false)}
         onOpen={() => setOpen(true)}
         disableSwipeToOpen={true}
         BackdropProps={{ open: false }}
-        style={{ position: 'relative' }}
+        sx={{
+          backgroundColor: 'transparent',
+          overflow: 'visible',
+          position: 'relative',
+          zIndex: 900,
+        }}
+        ModalProps={{
+          keepMounted: true,
+        }}
       >
-        <Box
-          sx={{
-            /*height: (t) => `calc(100vh - ${t.spacing(18)})`,*/
-            height: '60vh',
-            backgroundColor: 'white',
-            overflowY: 'auto',
-          }}
-        >
-          {children}
-        </Box>
+        <Box>{children}</Box>
       </SwipeableDrawer>
       {!open && (
         <Paper
-          sx={{ pointerEvents: 'auto' }}
-          className={classes.bottomPaper}
+          sx={{
+            pointerEvents: 'auto',
+            width: '100%',
+            position: 'fixed',
+            bottom: 0,
+            boxShadow: '0 -1px 2px rgb(0 0 0 / 30%)',
+            zIndex: 1000,
+            borderRadius: '8px 8px 0 0 ',
+          }}
           onClick={handleClickBottom}
         >
-          <Grid container className={classes.bottomBox} wrap="nowrap" pt={1}>
-            <Grid item className={classes.bottomArrow}>
+          <Grid
+            container
+            sx={{
+              height: 50,
+            }}
+            wrap="nowrap"
+            pt={1}
+          >
+            <Grid
+              item
+              p={1}
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
               <ExpandLess color="action" />
             </Grid>
-            <Grid item className={classes.bottomContent}>
-              <Grid container className={classes.box1}>
-                <Grid item className={classes.bottomItem}>
-                  <Avatar className={classes.avatar} />
+            <Grid
+              item
+              sx={{
+                flexGrow: 1,
+              }}
+            >
+              <Grid
+                container
+                sx={{
+                  height: '100%',
+                  alignItems: 'center',
+                  width: '100%',
+                }}
+              >
+                <Grid item ml={1}>
+                  <Avatar width={3} height={3} />
                 </Grid>
-                <Grid item className={classes.bottomItem}>
+                <Grid item ml={1}>
                   <Typography variant="h6">@$</Typography>
                 </Grid>
-                <Grid item className={classes.bottomItem}>
+                <Grid item ml={1}>
                   <Typography variant="body1">tokens</Typography>
                 </Grid>
               </Grid>
@@ -139,4 +111,4 @@ function Drawer(props) {
   );
 }
 
-export default withStyles(style)(Drawer);
+export default Drawer;
