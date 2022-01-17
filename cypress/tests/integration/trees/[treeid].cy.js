fc5741d82510e66d032066359e06847d84f5da81
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-01-17 12:23:45 -0800

    test: reduce [treeid] image test cases

--- cypress/tests/integration/trees/[treeid].cy.js
+++ cypress/tests/integration/trees/[treeid].cy.js
@@ -13,21 +13,7 @@ const exampleTreeData = {
 };
 
 const imageFixturesDir = 'images/trees/';
-const imageFixtureNames = [
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  '11',
-  '12',
-  '13',
-];
+const imageFixtureNames = ['1', '2', 'wide'];
 
 const testImage = (fileName, imageType = 'image/jpg') => {
   const imageFixturePath = `${imageFixturesDir}${fileName}`;
