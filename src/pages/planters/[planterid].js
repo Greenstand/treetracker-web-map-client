eb608f4e61be01b28ead92cfb0dd1af3ddc62a49
jwgatt <79326421+jwgatt@users.noreply.github.com>
2022-02-07 13:15:53 +0100

    Refine planter layout

--- src/pages/planters/[planterid].js
+++ src/pages/planters/[planterid].js
@@ -100,8 +100,8 @@ export default function Planter({ planter }) {
         variant="rounded"
         sx={{
           width: '100%',
-          height: '688px',
-          borderRadius: 6,
+          height: '600px',
+          borderRadius: 35,
           mt: 11,
           mb: [6, 10],
         }}
