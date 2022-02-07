ab7bb2c98e1cb32017450ab3d5e3a87587ab91ea
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-02-07 01:57:36 -0800

    docs: describe nextjs with msw idiosyncs

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -1,8 +1,12 @@
 /* eslint-disable global-require */
+
+// use correct mock depending on environment
 if (typeof window === 'undefined') {
+  // mock api calls from nextjs server
   const { server } = require('./server');
   server.listen();
 } else {
+  // mock api calls from nextjs client
   const { worker } = require('./browser');
   worker.start();
 }
