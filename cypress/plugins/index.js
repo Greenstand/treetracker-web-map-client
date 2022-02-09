bae9d0fdb5113a7f442ae11e4b9bd4d01cbb8227
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-02-08 17:25:18 -0800

    chore: remove separate component-testing cypress plugin file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,11 +1,13 @@
 /* eslint-disable global-require */
+const injectDevServer = require('@cypress/react/plugins/next');
 
 // start the Next.js server when Cypress starts
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (_on, config) => {
+module.exports = (on, config) => {
   require('cypress-watch-and-reload/plugins')(config);
+  injectDevServer(on, config);
 
   return config;
 };
