d872f1a997e86db4161ea70f2c70e23df7848179
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-02-08 16:46:07 -0800

    chore: remove nock

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,93 +1,11 @@
 /* eslint-disable global-require */
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable no-console */
-const http = require('http');
-const next = require('next');
-const nock = require('nock');
-require('dotenv').config();
 
 // start the Next.js server when Cypress starts
-module.exports = async (on, config) => {
+/**
+ * @type {Cypress.PluginConfig}
+ */
+module.exports = (_on, config) => {
   require('cypress-watch-and-reload/plugins')(config);
-
-  // copy env vars
-  // eslint-disable-next-line no-param-reassign
-  config.env.NEXT_PUBLIC_API_NEW = process.env.NEXT_PUBLIC_API_NEW;
-
-  // exit if not using nock to mock nextjs ssr functions
-  if (!config.env.nock) return config;
-
-  const app = next({ dev: true });
-  const handleNextRequests = app.getRequestHandler();
-  await app.prepare();
-
-  const customServer = new http.Server((req, res) =>
-    handleNextRequests(req, res),
-  );
-
-  await new Promise((resolve, reject) => {
-    customServer.listen(3000, (err) => {
-      if (err) {
-        return reject(err);
-      }
-      console.log('> Ready on http://localhost:3000');
-      return resolve();
-    });
-  });
-
-  // register handlers for cy.task command
-  // https://on.cypress.io/task
-  on('task', {
-    clearNock() {
-      nock.restore();
-      nock.cleanAll();
-
-      return null;
-    },
-
-    nock({ hostname, method, path, statusCode, body }) {
-      nock.activate();
-
-      console.log(
-        'nock will: %s %s%s respond with %d %o',
-        method,
-        hostname,
-        path,
-        statusCode,
-        body,
-      );
-
-      // add one-time network stub like
-      // nock('https://icanhazdadjoke.com').get('/').reply(200, ...)
-      const methodString = method.toLowerCase();
-      nock(hostname)[methodString](path).reply(statusCode, body);
-
-      return null;
-    },
-    nocks({ hostname, routes }) {
-      nock.activate();
-
-      let scope = nock(hostname);
-
-      for (const route of routes) {
-        const { method, path, statusCode, body } = route;
-
-        console.log(
-          'nock will: %s %s%s respond with %d %o',
-          method,
-          hostname,
-          path,
-          statusCode,
-          body,
-        );
-
-        const methodString = method.toLowerCase();
-        scope = scope[methodString](path).reply(statusCode, body);
-      }
-
-      return null;
-    },
-  });
 
   return config;
 };
