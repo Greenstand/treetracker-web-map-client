5469697e96391d26f414d21864351965fe70ec6f
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-02-12 19:37:35 -0800

    test: refactor treeid page

--- cypress/tests/integration/trees/[treeid].cy.js
+++ cypress/tests/integration/trees/[treeid].cy.js
@@ -1,29 +1,19 @@
 import exampleTreeData from '../../../fixtures/tree186734.json';
 import { prepareNocks, clearNocks } from '../nockRoutes';
 
-const imageFixturesDir = 'images/trees/';
-const imageFixtureNames = ['1'];
-
-const testImage = (fileName, imageType = 'image/jpg') => {
-  const imageFixturePath = `${imageFixturesDir}${fileName}`;
-  return it(`Image case - ${fileName}`, () => {
-    const treePath = `/trees/${exampleTreeData.id}`;
-    cy.fixture(imageFixturePath).then((image) => {
-      const blob = Cypress.Blob.base64StringToBlob(image, imageType);
-      const url = Cypress.Blob.createObjectURL(blob);
-      prepareNocks({
-        tree: { ...exampleTreeData, image_url: url },
-      });
-    });
-    cy.visit(treePath);
-    cy.contains(`${exampleTreeData.id}`);
-  });
-};
-
 beforeEach(() => {
   clearNocks();
 });
 
-describe('Image cases', () => {
-  imageFixtureNames.forEach(testImage);
+it(`treeid page`, () => {
+  const treePath = `/trees/${exampleTreeData.id}`;
+  cy.fixture('images/trees/1.jpg').then((image) => {
+    const blob = Cypress.Blob.base64StringToBlob(image, 'image/jpg');
+    const url = Cypress.Blob.createObjectURL(blob);
+    prepareNocks({
+      tree: { ...exampleTreeData, image_url: url },
+    });
+  });
+  cy.visit(treePath);
+  cy.contains(`${exampleTreeData.id}`);
 });
