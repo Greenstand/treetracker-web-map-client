215b4cefd348a296b54138e9e7ee3030c3bb010a
RubenSmn <saaman.ruben@gmail.com>
2022-06-24 15:38:10 +0200

    refactor: style ui according to design

--- src/components/TreeInfoDialog.js
+++ src/components/TreeInfoDialog.js
@@ -1,6 +1,4 @@
 import CloseIcon from '@mui/icons-material/Close';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import ShareIcon from '@mui/icons-material/Share';
 import {
   Box,
   Grid,
@@ -21,6 +19,8 @@ import {
   useTheme,
 } from '@mui/material';
 import { useState } from 'react';
+import heartIcon from '../images/icons/heart.svg';
+import shareIcon from '../images/icons/share-icon.svg';
 import maxIcon from '../images/max.svg';
 import { makeStyles } from '../models/makeStyles';
 
@@ -34,13 +34,90 @@ const useStyles = makeStyles()(() => ({
   },
 }));
 
+function CustomListText(props) {
+  const { primary, secondary } = props;
+  return (
+    <ListItemText
+      primary={
+        <Typography
+          sx={{
+            fontSize: '12px',
+            fontWeight: 400,
+            letterSpacing: '0.04em',
+          }}
+        >
+          {primary}
+        </Typography>
+      }
+      secondary={
+        <Typography
+          sx={{
+            fontSize: '16px',
+            fontWeight: 700,
+            letterSpacing: '0.02em',
+          }}
+        >
+          {secondary}
+        </Typography>
+      }
+    />
+  );
+}
+
+function CustomListAvatar(props) {
+  const { src, alt } = props;
+  return (
+    <ListItemAvatar
+      sx={{
+        height: 56,
+        width: 56,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'start',
+      }}
+    >
+      <Avatar
+        src={src}
+        alt={alt}
+        sx={{
+          height: 48,
+          width: 48,
+        }}
+      />
+    </ListItemAvatar>
+  );
+}
+
+function CustomImageItem(props) {
+  const { src, alt, isActive } = props;
+  const { classes } = useStyles();
+  return (
+    <ImageListItem
+      sx={{
+        background: (t) => (isActive ? t.palette.primary.main : ''),
+        borderRadius: 6,
+        maxWidth: '132px',
+        p: 2,
+      }}
+    >
+      <img
+        src={src}
+        srcSet={src}
+        alt={alt}
+        loading="lazy"
+        className={classes.imageSmall}
+      />
+    </ImageListItem>
+  );
+}
+
 export default function TreeInfoDialog(props) {
   const { tree, planter, organization } = props;
   const { classes } = useStyles();
+  /* eslint-disable no-shadow */
   const theme = useTheme();
   const [open, setOpen] = useState(false);
   const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
-  const isLgOrSmaller = useMediaQuery(theme.breakpoints.down('lg'));
 
   const handleOpen = () => {
     setOpen(true);
@@ -67,15 +144,25 @@ export default function TreeInfoDialog(props) {
       <Dialog
         fullScreen={fullScreen}
         fullWidth
-        maxWidth="lg"
         open={open}
         onClose={handleClose}
+        scroll={fullScreen ? 'paper' : 'body'}
+        PaperProps={{
+          sx: {
+            borderRadius: { sm: 0, md: 4 },
+            mt: { xs: 18, md: 16 },
+            mx: 0,
+            maxWidth: 1,
+            width: '100vw',
+          },
+        }}
         sx={{
-          mt: '72px',
+          py: { xs: 9, md: 4 },
+          fontFamily: 'Lato',
+          height: { xs: 'calc(100vh - 36)' },
         }}
       >
         <DialogTitle>
-          <Typography>Tree Information Details</Typography>
           <IconButton
             aria-label="close"
             onClick={handleClose}
@@ -89,34 +176,47 @@ export default function TreeInfoDialog(props) {
             <CloseIcon />
           </IconButton>
         </DialogTitle>
-        <DialogContent>
-          <Grid container columns={{ sm: 1, md: 4 }} spacing={6}>
+        <DialogContent
+          sx={{
+            pb: 9,
+          }}
+        >
+          <Grid
+            container
+            columns={{ sm: 1, md: 4 }}
+            spacing={6}
+            sx={{ pb: { sm: 6, md: 0 } }}
+          >
             <Grid item md={1}>
-              <ImageList cols={isLgOrSmaller ? 1 : 2} rowHeight={156}>
-                <ImageListItem
-                  sx={{
-                    background: (t) => t.palette.primary.main,
-                    borderRadius: 6,
-                    p: 2,
-                  }}
-                >
-                  <img
-                    src={`${tree.image_url}`}
-                    srcSet={`${tree.image_url}`}
-                    alt={`tree - #${tree.id}`}
-                    loading="lazy"
-                    className={classes.imageSmall}
-                  />
-                </ImageListItem>
+              <ImageList
+                rowHeight={156}
+                gap={8}
+                sx={{
+                  gridTemplateColumns: {
+                    xs: 'repeat(3, 1fr) !important',
+                    sm: 'repeat(4, 1fr) !important',
+                    md: 'repeat(1, 1fr) !important',
+                    lg: 'repeat(2, 1fr) !important',
+                    xl: 'repeat(3, 1fr) !important',
+                  },
+                  justifyItems: 'center',
+                  my: 0,
+                }}
+              >
+                <CustomImageItem
+                  isActive
+                  src={tree.image_url}
+                  alt={`tree - #${tree.id}`}
+                />
               </ImageList>
             </Grid>
             <Grid item md={2}>
               <Box
                 sx={{
                   maxWidth: 1,
-                  maxHeight: [332, 664],
+                  maxHeight: [512, 'calc(100vh - 196px)'],
                   overflow: 'hidden',
-                  borderRadius: 8,
+                  borderRadius: 4,
                 }}
               >
                 <img
@@ -135,54 +235,28 @@ export default function TreeInfoDialog(props) {
               }}
             >
               <Box sx={{ flex: '1' }}>
-                <Typography variant="h4">Palm Tree - {tree.id}</Typography>
+                <Typography variant="h4" fontSize="24px">
+                  Palm Tree - {tree.id}
+                </Typography>
                 <Typography variant="h6">Eco-Peace-Vision</Typography>
                 <List>
                   <ListItem sx={{ pl: 0 }}>
-                    <ListItemAvatar
-                      sx={{
-                        height: 56,
-                        width: 56,
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'start',
-                      }}
-                    >
-                      <Avatar
-                        src={planter.image_url}
-                        alt={`${planter.first_name} ${planter.last_name}`}
-                        sx={{
-                          height: 48,
-                          width: 48,
-                        }}
-                      />
-                    </ListItemAvatar>
-                    <ListItemText
+                    <CustomListAvatar
+                      src={planter.image_url}
+                      alt={`${planter.first_name} ${planter.last_name}`}
+                    />
+                    <CustomListText
                       primary="Planter"
                       secondary={`${planter.first_name} ${planter.last_name}`}
                     />
                   </ListItem>
                   {organization && (
                     <ListItem sx={{ pl: 0 }}>
-                      <ListItemAvatar
-                        sx={{
-                          height: 56,
-                          width: 56,
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'start',
-                        }}
-                      >
-                        <Avatar
-                          src={organization.logo_url}
-                          alt={organization.name}
-                          sx={{
-                            height: 48,
-                            width: 48,
-                          }}
-                        />
-                      </ListItemAvatar>
-                      <ListItemText
+                      <CustomListAvatar
+                        src={organization.logo_url}
+                        alt={organization.name}
+                      />
+                      <CustomListText
                         primary="Planting Organization"
                         secondary={organization.name}
                       />
@@ -197,28 +271,35 @@ export default function TreeInfoDialog(props) {
                 }}
               >
                 <Button
-                  startIcon={<FavoriteBorderIcon sx={{ color: '#fff' }} />}
+                  startIcon={<img src={heartIcon} />}
+                  disableElevation
                   variant="contained"
                   color="primary"
                   sx={{
                     py: 3,
                     borderRadius: 3,
                     color: '#fff',
+                    fontSize: '12px',
+                    fontWeight: 400,
+                    letterSpacing: '0.04em',
                   }}
                 >
                   200
                 </Button>
                 <Button
-                  startIcon={
-                    <ShareIcon
-                      sx={{ color: (t) => t.palette.text.primaryLight }}
-                    />
-                  }
-                  color="background"
+                  startIcon={<img src={shareIcon} />}
+                  disableElevation
                   variant="contained"
+                  color="background"
                   sx={{
+                    color: 'text.text1',
                     py: 3,
                     borderRadius: 3,
+                    backgroundColor: '#e1e2e2',
+                    textTransform: 'none',
+                    fontSize: '12px',
+                    fontWeight: 400,
+                    letterSpacing: '0.04em',
                   }}
                 >
                   Share
