1f49e44a33450bb306fb2c0f4e5887f92b31132a
RubenSmn <saaman.ruben@gmail.com>
2022-08-05 14:36:31 +0200

    feat(playground): add color picker

--- src/components/playground/ColorInput.js
+++ src/components/playground/ColorInput.js
@@ -1,6 +1,15 @@
-import { Box, TextField } from '@mui/material';
-import { useState, useEffect } from 'react';
+import ColorizeIcon from '@mui/icons-material/Colorize';
+import {
+  Box,
+  TextField,
+  IconButton,
+  InputAdornment,
+  Popover,
+} from '@mui/material';
+import { useState, useEffect, useRef } from 'react';
+import { HexColorPicker } from 'react-colorful';
 import { usePlaygroundUtils } from '../../hooks/contextHooks';
+import useDisclosure from '../../hooks/useDisclosure';
 import { propRules } from '../../models/themePlaygroundOptions';
 
 function ColorInput(props) {
@@ -10,6 +19,9 @@ function ColorInput(props) {
   const [value, setValue] = useState(initialValue);
   const [isValid, setValid] = useState(true);
   const isGradient = /gradient/i.test(label);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const pickerRef = useRef(null);
+  let timer = null;
 
   useEffect(() => {
     setValue(initialValue);
@@ -23,6 +35,14 @@ function ColorInput(props) {
     if (!isGradient) if (!propRules.color.test(userValue)) return;
     setValid(true);
     setPropByPath(path, userValue);
+  };
+
+  const handleColorChange = (color) => {
+    if (timer) clearTimeout(timer);
+    // if the user drags too fast the state can't keep up
+    timer = setTimeout(() => {
+      setPropByPath(path, color);
+    }, 300);
   };
 
   return (
@@ -43,7 +63,31 @@ function ColorInput(props) {
           width: 1,
         }}
         helperText={!isValid && 'Invalid syntax'}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={onOpen} ref={pickerRef}>
+                <ColorizeIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
       />
+      <Popover
+        open={isOpen}
+        onClose={onClose}
+        anchorEl={pickerRef.current}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <HexColorPicker color={value} onChange={handleColorChange} />
+      </Popover>
     </Box>
   );
 }
