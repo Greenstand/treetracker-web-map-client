bce2df449fea152f074e76cf20c0458f65fc61ec
Mohmn <naqashmohmin1@gmail.com>
2022-08-13 01:28:37 +0530

    feat: updated test

--- src/components/playground/FontAddInput.cy.js
+++ src/components/playground/FontAddInput.cy.js
@@ -18,41 +18,10 @@ describe('Toggle Theme Mode', () => {
       </PlaygroundProvider>,
     );
     cy.get('.MuiAccordionSummary-content').click();
-    cy.get('.MuiFormControl-root').eq(0).type('lora');
-    cy.get('.MuiFormControl-root').eq(1).type('700');
-    cy.get('button').click();
+    cy.get('.MuiFormControl-root').eq(0).type('lora{enter}');
     cy.get('.MuiFormControl-root')
       .eq(0)
       .contains('Font could not be loaded')
-      .should('not.exist');
-  });
-
-  it('multiple scenarios for loading font', () => {
-    mount(
-      <PlaygroundProvider>
-        <FontAddInput />
-      </PlaygroundProvider>,
-    );
-    cy.get('.MuiAccordionSummary-content').click();
-    cy.get('.MuiFormControl-root').eq(0).type('lora');
-    cy.get('.MuiFormControl-root').eq(1).type('700');
-    cy.get('button').click();
-
-    cy.get('.MuiFormControl-root input').eq(0).clear();
-    cy.get('.MuiFormControl-root input').eq(1).clear();
-    cy.get('.MuiFormControl-root').eq(0).type('lora');
-    cy.get('.MuiFormControl-root').eq(1).type('700');
-    cy.get('button').click();
-    cy.get('.MuiFormControl-root').eq(0).contains('Font Already Loaded');
-
-    cy.get('.MuiFormControl-root input').eq(0).clear();
-    cy.get('.MuiFormControl-root input').eq(1).clear();
-    cy.get('.MuiFormControl-root').eq(0).type('lora');
-    cy.get('.MuiFormControl-root').eq(1).type(' 700,400 ');
-    cy.get('button').click();
-    cy.get('.MuiFormControl-root')
-      .eq(0)
-      .contains('Font Already Loaded')
       .should('not.exist');
   });
 
@@ -63,8 +32,7 @@ describe('Toggle Theme Mode', () => {
       </PlaygroundProvider>,
     );
     cy.get('.MuiAccordionSummary-content').click();
-    cy.get('.MuiFormControl-root').eq(0).type('void void vpid');
-    cy.get('button').click();
+    cy.get('.MuiFormControl-root').eq(0).type('void void vpid{enter}');
     cy.get('.MuiFormControl-root').eq(0).contains('Font could not be loaded');
   });
 });
