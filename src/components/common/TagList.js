09e1f771f1d3d1bbde842b925702d76bc5aa6098
RubenSmn <saaman.ruben@gmail.com>
2022-09-07 13:43:34 +0200

    chore: add comments for taglist

--- src/components/common/TagList.js
+++ src/components/common/TagList.js
@@ -1,13 +1,18 @@
 import { Box } from '@mui/material';
 
+// Used to display the TreeTag component in a list
+// will define the postion(head/tail) based on the disabled prop on the TreeTag
 function TagList({ children }) {
   const tagsHead = [];
   const tagsTail = [];
 
+  // check for every TreeTag if disabled
   children.forEach((tag) => {
     if (tag.props.disabled) {
+      // push to the end of the list
       tagsTail.push(tag);
     } else {
+      // push to the front of the list
       tagsHead.push(tag);
     }
   });
