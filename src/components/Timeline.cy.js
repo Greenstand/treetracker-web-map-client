1f80dfce252a2f743a147105325174603629a1c4
RubenSmn <saaman.ruben@gmail.com>
2022-09-08 14:09:03 +0200

    fix: timeline router not working in test

--- src/components/Timeline.cy.js
+++ src/components/Timeline.cy.js
@@ -1,13 +1,39 @@
+import * as NextRouter from 'next/router';
 import Timeline from './Timeline';
 import { mountWithTheme as mount } from '../models/test-utils';
 
 describe('Timeline', () => {
-  it('renders on desktop', () => {
-    mount(<Timeline />);
+  describe('given page is top', () => {
+    beforeEach(() => {
+      const asPath = '/top';
+      cy.stub(NextRouter, 'useRouter').returns({ asPath });
+    });
+
+    it('should render on desktop', () => {
+      mount(<Timeline />);
+    });
+
+    it('should render on mobile', () => {
+      cy.viewport('iphone-6');
+      mount(<Timeline />);
+    });
   });
 
-  it('renders on mobile', () => {
-    cy.viewport('iphone-6');
-    mount(<Timeline />);
+  describe('given page is not top', () => {
+    beforeEach(() => {
+      const asPath = '/planters';
+      cy.stub(NextRouter, 'useRouter').returns({ asPath });
+    });
+
+    it('should not render on desktop', () => {
+      mount(<Timeline />);
+      cy.get('.MuiBox-root').should('not.exist');
+    });
+
+    it('should not render on mobile', () => {
+      cy.viewport('iphone-6');
+      mount(<Timeline />);
+      cy.get('.MuiBox-root').should('not.exist');
+    });
   });
 });
