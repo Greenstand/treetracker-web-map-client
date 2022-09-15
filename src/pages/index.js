fda09c89d5d0b9a5254a30650c7699de958a2b21
deanchen <antlet2004@gmail.com>
2022-09-16 05:58:17 +0800

    fix: no data on emebed home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default function Homepage({ nextExtraIsEmbed }) {
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <title>Treetracker by Greenstand</title>
       </Head>
-      {nextExtraIsEmbed || <Home />}
+      <Home />
     </>
   );
 }
