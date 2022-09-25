aca17f4c2452dbbe8e3992b77f97c9645a25cd38
RubenSmn <saaman.ruben@gmail.com>
2022-09-25 12:40:17 +0200

    refactor(custom-card): update theme logic

--- src/components/common/CustomCard.js
+++ src/components/common/CustomCard.js
@@ -3,25 +3,17 @@ import { Box, SvgIcon } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
-import { useTheme } from '@mui/material/styles';
-import { makeStyles } from '../../models/makeStyles';
-
-const useStyles = makeStyles()((theme) => ({}));
 
 function CustomCard(props) {
-  const { classes } = useStyles(props);
-  const theme = useTheme();
   const { iconURI, title, text, handleClick, disabled, tooltip, sx } = props;
 
   return (
     <Box
-      className={classes.root}
       onClick={handleClick}
       elevation={0}
       sx={{
-        background: disabled
-          ? theme.palette.grey[300]
-          : theme.palette.background.greenGradient,
+        background: ({ palette }) =>
+          disabled ? palette.grey[300] : palette.background.greenGradient,
         backgroundColor: disabled ? '' : 'common.white',
         borderRadius: [2, 4],
         p: [3, 6],
@@ -32,16 +24,16 @@ function CustomCard(props) {
     >
       <Box sx={{}}>
         <Avatar
-          className={classes.avatar}
           sx={{
             width: [36, 60],
             height: [36, 60],
             bgcolor: 'background.paper',
             boxShadow: disabled ? '' : '0px 6px 12px 0px #585B5D40',
-            color: disabled
-              ? // ? theme.palette.darkGrey.main
-                theme.palette.success.main
-              : theme.palette.success.main,
+            color: ({ palette }) =>
+              disabled
+                ? // ? theme.palette.darkGrey.main
+                  palette.success.main
+                : palette.success.main,
           }}
         >
           <SvgIcon
