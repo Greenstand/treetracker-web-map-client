da8f8b3e8544c39a67ac64ff6f874eeb8cee90f6
khalatevarun <khalatevarun@gmail.com>
2022-09-27 23:11:17 +0530

    feat: change padding

--- src/components/TreeSpeciesCard.js
+++ src/components/TreeSpeciesCard.js
@@ -38,7 +38,6 @@ function TreeSpeciesCard(props) {
           sx={{
             wordWrap: 'break-word',
             width: '65%',
-            padding: '8px 0',
           }}
         >
           <Typography
@@ -78,7 +77,7 @@ function TreeSpeciesCard(props) {
           sx={{
             background: (t) => t.palette.background.paperDark,
             color: (t) => t.palette.text.primaryReverse,
-            padding: (t) => [t.spacing(4, 5), t.spacing(6, 8)],
+            padding: (t) => [t.spacing(5), t.spacing(6, 8)],
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
