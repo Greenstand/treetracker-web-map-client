118ca8e131240daab254551c1362eac9aca6d3ec
khalatevarun <khalatevarun@gmail.com>
2022-09-28 00:49:49 +0530

    feat: shadow to header

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -280,6 +280,8 @@ export default function Drawer(props) {
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
+            boxShadow:
+              'rgb(0 0 0 / 20%) 0px 2px 4px -1px, rgb(0 0 0 / 14%) 0px 4px 5px 0px, rgb(0 0 0 / 12%) 0px 1px 10px 0px',
           }}
         >
           <Box
