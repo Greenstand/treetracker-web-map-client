cb977558cf1c77cfa47f4e7d42b5e4720220aeb1
khalatevarun <khalatevarun@gmail.com>
2022-09-28 00:50:51 +0530

    feat: add comment

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -281,7 +281,7 @@ export default function Drawer(props) {
             justifyContent: 'center',
             alignItems: 'center',
             boxShadow:
-              'rgb(0 0 0 / 20%) 0px 2px 4px -1px, rgb(0 0 0 / 14%) 0px 4px 5px 0px, rgb(0 0 0 / 12%) 0px 1px 10px 0px',
+              'rgb(0 0 0 / 20%) 0px 2px 4px -1px, rgb(0 0 0 / 14%) 0px 4px 5px 0px, rgb(0 0 0 / 12%) 0px 1px 10px 0px', // box shadow prop in parity with navbar shadow
           }}
         >
           <Box
