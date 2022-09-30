72ef64c2d36fba16ddb6ae3691b1877f01540cd2
dharmiknotop <dhardharmik7@gmail.com>
2022-09-30 17:30:58 +0530

    fix: button element was not taking the full width made correct by adding width 100%

--- src/components/Link.js
+++ src/components/Link.js
@@ -22,7 +22,7 @@ const NextComposed = React.forwardRef((props, ref) => {
   return (
     <NextLink href={href} as={as}>
       <a
-        style={{ display: 'inline-flex' }}
+        style={{ display: 'inline-flex', width: '100%' }}
         ref={ref}
         {...other}
         className={classes.link}
