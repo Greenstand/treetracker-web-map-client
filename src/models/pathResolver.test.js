e5941c5cac1895411ac8cb63e92677ed656ed93e
Harry Tran <huytran.quang080199@gmail.com>
2022-10-05 21:18:47 +1000

    fix: unit test path resolver

--- src/models/pathResolver.test.js
+++ src/models/pathResolver.test.js
@@ -67,9 +67,7 @@ describe('Test pathResolver', () => {
 
       expect(result).toMatchObject({
         pathname: '/wallets/0cdf4219-869a-41ce-953a-a8421d8353f7/tokens',
-        query: {
-          tree_id: 14615,
-        },
+        query: {},
       });
     });
 
@@ -137,7 +135,7 @@ describe('Test pathResolver', () => {
       );
 
       expect(result).toMatchObject({
-        pathname: '/trees/14615',
+        pathname: '/wallets/0cdf4219-869a-41ce-953a-a8421d8353f7/tokens',
         query: {},
       });
     });
