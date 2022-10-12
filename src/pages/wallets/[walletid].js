cf3f4740fe34d575264043217dbed66c99a01c78
khalatevarun <khalatevarun@gmail.com>
2022-10-13 03:36:34 +0530

    feat: add image for wallet page map

--- src/pages/wallets/[walletid].js
+++ src/pages/wallets/[walletid].js
@@ -45,7 +45,7 @@ const placeholderText = `Lorem ipsum dolor sit amet consectetur adipisicing elit
 export default function Wallet(props) {
   log.info('props for wallet page:', props);
   const [isTokenTab, setIsTokenTab] = React.useState(false);
-  const { wallet, species, tokens, trees } = props;
+  const { wallet, species, tokens, trees, nextExtraIsEmbed } = props;
   const isMobile = useMobile();
   // eslint-disable-next-line react/destructuring-assignment
   const tokenRegionStatistics = props.tokenRegionCount.filter(
@@ -98,92 +98,119 @@ export default function Wallet(props) {
     reload();
   }, [mapContext, wallet]);
   return (
-    <Box
-      sx={[
-        {
-          padding: (t) => [t.spacing(0, 4), 6],
-          width: 1,
-          boxSizing: 'border-box',
-          // overflow : 'scroll'
-        },
-      ]}
-    >
-      {!isMobile && (
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            width: '100%',
-            alignItems: 'center',
-          }}
-        >
-          <Crumbs
-            items={[
-              {
-                // icon: <HomeIcon />,
-                name: 'Home',
-                url: '/',
-              },
-              {
-                icon: wallet.logo_url || (
-                  <Icon icon={AccountBalanceWalletIcon} />
-                ),
-                name: `${wallet.name}`,
-              },
-            ]}
-          />
-          <Box>
-            <Icon
-              icon={SearchIcon}
-              width={48}
-              height={48}
-              color="grey"
-              sx={{
-                fill: 'transparent',
-                '& path': {
-                  fill: 'grey',
-                },
-              }}
-            />
-          </Box>
-        </Box>
-      )}
-
+    <>
       <Box
-        sx={{
-          borderRadius: 4,
-          mt: 6,
-          '& img': {
-            width: '100%',
-          },
-        }}
-      >
-        <ProfileCover src={wallet?.cover_url || trees?.[0]?.image_url} />
-        <Avatar
-          src={wallet.logo_url || imagePlaceholder}
-          sx={{
-            width: [120, 189],
-            height: [120, 189],
-            borderWidth: [4, 9],
-            borderStyle: 'solid',
-            borderColor: (t) => t.palette.background.paper,
+        sx={[
+          {
+            padding: (t) => [t.spacing(0, 4), 6],
+            width: 1,
             boxSizing: 'border-box',
-            ml: [4, 8],
-            mt: [-98 / 4, -146 / 4],
-          }}
-        />
-      </Box>
-
-      {isMobile && (
-        <Portal
-          container={() => document.getElementById('drawer-title-container')}
-        >
+            // overflow : 'scroll'
+          },
+        ]}
+      >
+        {!isMobile && (
           <Box
             sx={{
-              px: 4,
-              pb: 4,
+              display: 'flex',
+              justifyContent: 'space-between',
+              width: '100%',
+              alignItems: 'center',
             }}
           >
+            <Crumbs
+              items={[
+                {
+                  // icon: <HomeIcon />,
+                  name: 'Home',
+                  url: '/',
+                },
+                {
+                  icon: wallet.logo_url || (
+                    <Icon icon={AccountBalanceWalletIcon} />
+                  ),
+                  name: `${wallet.name}`,
+                },
+              ]}
+            />
+            <Box>
+              <Icon
+                icon={SearchIcon}
+                width={48}
+                height={48}
+                color="grey"
+                sx={{
+                  fill: 'transparent',
+                  '& path': {
+                    fill: 'grey',
+                  },
+                }}
+              />
+            </Box>
+          </Box>
+        )}
+
+        <Box
+          sx={{
+            borderRadius: 4,
+            mt: 6,
+            '& img': {
+              width: '100%',
+            },
+          }}
+        >
+          <ProfileCover src={wallet?.cover_url || trees?.[0]?.image_url} />
+          <Avatar
+            src={wallet.logo_url || imagePlaceholder}
+            sx={{
+              width: [120, 189],
+              height: [120, 189],
+              borderWidth: [4, 9],
+              borderStyle: 'solid',
+              borderColor: (t) => t.palette.background.paper,
+              boxSizing: 'border-box',
+              ml: [4, 8],
+              mt: [-98 / 4, -146 / 4],
+            }}
+          />
+        </Box>
+
+        {isMobile && (
+          <Portal
+            container={() => document.getElementById('drawer-title-container')}
+          >
+            <Box
+              sx={{
+                px: 4,
+                pb: 4,
+              }}
+            >
+              <Typography variant="h2">{wallet.name} </Typography>
+              <Box sx={{ mt: 2 }}>
+                <Info
+                  iconURI={CalendarIcon}
+                  info={`Wallet created on ${moment(wallet.created_at).format(
+                    'MMMM DD, YYYY',
+                  )}`}
+                />
+              </Box>
+            </Box>
+          </Portal>
+        )}
+        {isMobile && (
+          <Portal
+            container={() =>
+              document.getElementById('drawer-title-container-min')
+            }
+          >
+            <Box sx={{}}>
+              <Typography variant="h3">{wallet.name} </Typography>
+            </Box>
+          </Portal>
+        )}
+
+        {!isMobile && (
+          <Box sx={{ mt: 6 }}>
             <Typography variant="h2">{wallet.name} </Typography>
             <Box sx={{ mt: 2 }}>
               <Info
@@ -194,165 +221,162 @@ export default function Wallet(props) {
               />
             </Box>
           </Box>
-        </Portal>
-      )}
-      {isMobile && (
-        <Portal
-          container={() =>
-            document.getElementById('drawer-title-container-min')
-          }
-        >
-          <Box sx={{}}>
-            <Typography variant="h3">{wallet.name} </Typography>
-          </Box>
-        </Portal>
-      )}
-
-      {!isMobile && (
-        <Box sx={{ mt: 6 }}>
-          <Typography variant="h2">{wallet.name} </Typography>
-          <Box sx={{ mt: 2 }}>
-            <Info
-              iconURI={CalendarIcon}
-              info={`Wallet created on ${moment(wallet.created_at).format(
-                'MMMM DD, YYYY',
-              )}`}
-            />
-          </Box>
-        </Box>
-      )}
-      <Box
-        sx={{
-          mt: [8, 16],
-        }}
-      >
-        <Typography variant="h4">Featured trees by {wallet.name}</Typography>
-        <FeaturedTreesSlider
-          trees={trees}
-          link={(item) => `/wallets/${wallet.id}/tokens/${item.token_id}`}
-        />
-      </Box>
-
-      <Grid
-        container
-        wrap="nowrap"
-        justifyContent="space-between"
-        sx={{
-          width: 1,
-          mt: [6, 12],
-        }}
-      >
-        <Grid item sx={{ width: '49%' }}>
-          <CustomCard
-            handleClick={() => setIsTokenTab(false)}
-            iconURI={TreeIcon}
-            iconProps={{
-              sx: {
-                '& path': {
-                  fill: ({ palette }) => palette.primary.main,
-                },
-              },
-            }}
-            title="Trees"
-            text={tokens.total}
-            disabled={isTokenTab}
-          />
-        </Grid>
-        <Grid item sx={{ width: '49%' }}>
-          <CustomCard
-            handleClick={() => setIsTokenTab(true)}
-            iconURI={TokenIcon}
-            iconProps={{
-              sx: {
-                '& path': {
-                  fill: ({ palette }) => palette.text.primary,
-                },
-              },
-            }}
-            title="Tokens"
-            text={tokens.total}
-            disabled={!isTokenTab}
-          />
-        </Grid>
-      </Grid>
-
-      {tokenRegionName.length > 0 && (
-        <Box sx={{ mt: [0, 22], display: !isTokenTab ? 'block' : 'none' }}>
-          <CustomWorldMap totalTrees={tokenRegionCount} con={tokenRegionName} />
-        </Box>
-      )}
-
-      <Box
-        sx={{ mt: [0, 16], p: [2, 4], display: isTokenTab ? 'block' : 'none' }}
-      >
-        {tokens.tokens.map((token) => (
-          <Box
-            key={token.id}
-            sx={{
-              mt: [2, 4],
-            }}
-          >
-            <TreeTag
-              TreeTagValue={token.id}
-              title="Token ID"
-              icon={<Icon icon={TokenIcon} />}
-              link={`/wallets/${wallet.id}/tokens/${token.id}`}
-            />
-          </Box>
-        ))}
-      </Box>
-
-      {species.length > 0 && (
+        )}
         <Box
           sx={{
-            px: [0, 6],
+            mt: [8, 16],
           }}
         >
-          <Typography
-            variant="h4"
-            sx={{
-              fontSize: [16, 24],
-              mt: [5, 10],
-            }}
-          >
-            Species of trees planted
-          </Typography>
+          <Typography variant="h4">Featured trees by {wallet.name}</Typography>
+          <FeaturedTreesSlider
+            trees={trees}
+            link={(item) => `/wallets/${wallet.id}/tokens/${item.token_id}`}
+          />
+        </Box>
+
+        <Grid
+          container
+          wrap="nowrap"
+          justifyContent="space-between"
+          sx={{
+            width: 1,
+            mt: [6, 12],
+          }}
+        >
+          <Grid item sx={{ width: '49%' }}>
+            <CustomCard
+              handleClick={() => setIsTokenTab(false)}
+              iconURI={TreeIcon}
+              iconProps={{
+                sx: {
+                  '& path': {
+                    fill: ({ palette }) => palette.primary.main,
+                  },
+                },
+              }}
+              title="Trees"
+              text={tokens.total}
+              disabled={isTokenTab}
+            />
+          </Grid>
+          <Grid item sx={{ width: '49%' }}>
+            <CustomCard
+              handleClick={() => setIsTokenTab(true)}
+              iconURI={TokenIcon}
+              iconProps={{
+                sx: {
+                  '& path': {
+                    fill: ({ palette }) => palette.text.primary,
+                  },
+                },
+              }}
+              title="Tokens"
+              text={tokens.total}
+              disabled={!isTokenTab}
+            />
+          </Grid>
+        </Grid>
+
+        {tokenRegionName.length > 0 && (
+          <Box sx={{ mt: [0, 22], display: !isTokenTab ? 'block' : 'none' }}>
+            <CustomWorldMap
+              totalTrees={tokenRegionCount}
+              con={tokenRegionName}
+            />
+          </Box>
+        )}
+
+        <Box
+          sx={{
+            mt: [0, 16],
+            p: [2, 4],
+            display: isTokenTab ? 'block' : 'none',
+          }}
+        >
+          {tokens.tokens.map((token) => (
+            <Box
+              key={token.id}
+              sx={{
+                mt: [2, 4],
+              }}
+            >
+              <TreeTag
+                TreeTagValue={token.id}
+                title="Token ID"
+                icon={<Icon icon={TokenIcon} />}
+                link={`/wallets/${wallet.id}/tokens/${token.id}`}
+              />
+            </Box>
+          ))}
+        </Box>
+
+        {species.length > 0 && (
           <Box
             sx={{
-              mt: [5, 10],
+              px: [0, 6],
             }}
           >
-            {species.map((specie) => (
-              <TreeSpeciesCard
-                key={specie.id}
-                name={specie.name}
-                count={specie.total}
-                subTitle={specie.desc || '---'}
-              />
-            ))}
+            <Typography
+              variant="h4"
+              sx={{
+                fontSize: [16, 24],
+                mt: [5, 10],
+              }}
+            >
+              Species of trees planted
+            </Typography>
+            <Box
+              sx={{
+                mt: [5, 10],
+              }}
+            >
+              {species.map((specie) => (
+                <TreeSpeciesCard
+                  key={specie.id}
+                  name={specie.name}
+                  count={specie.total}
+                  subTitle={specie.desc || '---'}
+                />
+              ))}
+            </Box>
           </Box>
-        </Box>
+        )}
+        <Divider
+          varian="fullwidth"
+          sx={{
+            mt: [10, 20],
+          }}
+        />
+        <Typography sx={{ mt: [2.5, 5] }} variant="h4">
+          About the Wallet
+        </Typography>
+        <Typography sx={{ mt: [2.5, 5] }} variant="body2">
+          {wallet.about || 'NO DATA YET'}
+        </Typography>
+        <Divider
+          varian="fullwidth"
+          sx={{
+            mt: [10, 20],
+          }}
+        />
+        <ImpactSection />
+      </Box>
+      {nextExtraIsEmbed && (
+        <Portal
+          container={() => document.getElementById('embed-logo-container')}
+        >
+          <Avatar
+            sx={{
+              width: '120px',
+              height: '120px',
+              margin: '10px',
+            }}
+            src={wallet.tree_image_url ?? imagePlaceholder}
+            variant="rounded"
+          />
+        </Portal>
       )}
-      <Divider
-        varian="fullwidth"
-        sx={{
-          mt: [10, 20],
-        }}
-      />
-      <Typography sx={{ mt: [2.5, 5] }} variant="h4">
-        About the Wallet
-      </Typography>
-      <Typography sx={{ mt: [2.5, 5] }} variant="body2">
-        {wallet.about || 'NO DATA YET'}
-      </Typography>
-      <Divider
-        varian="fullwidth"
-        sx={{
-          mt: [10, 20],
-        }}
-      />
-      <ImpactSection />
-    </Box>
+    </>
   );
 }
 
