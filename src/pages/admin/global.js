6b6e64a8b1ce12a33813bbc678f2adfdb5007aa4
RubenSmn <saaman.ruben@gmail.com>
2022-10-18 14:56:28 +0200

    feat(dashboard): add basic setup

--- src/pages/admin/global.js
+++ src/pages/admin/global.js
@@ -1,7 +1,99 @@
+import {
+  Box,
+  Typography,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+} from '@mui/material';
 import log from 'loglevel';
+import { useState } from 'react';
+
+const green = '#61892F';
+const lightGrey = '#f5f5f3';
+
+function Tab({ children, value, index, onClick }) {
+  return (
+    <ListItem
+      onClick={() => onClick(index)}
+      sx={{
+        cursor: 'pointer',
+        boxSizing: 'border-box',
+        borderLeft:
+          value === index ? `2px solid ${green}` : `2px solid ${lightGrey}`,
+      }}
+    >
+      {children}
+    </ListItem>
+  );
+}
+
+function TabPanel({ children, value, index }) {
+  return value === index ? children : null;
+}
 
 function Global() {
-  return <h1>global setting for web map</h1>;
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const handleSidebarClick = (index) => {
+    setCurrentTab(index);
+  };
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        height: '100vh',
+      }}
+    >
+      <Box
+        sx={{
+          minWidth: '220px',
+          p: 2,
+          backgroundColor: lightGrey,
+        }}
+      >
+        <Typography
+          variant="h6"
+          sx={{
+            textAlign: 'center',
+          }}
+        >
+          Dashboard
+        </Typography>
+        <Divider
+          sx={{
+            my: 2,
+          }}
+        />
+        <List
+          sx={{
+            p: 0,
+          }}
+        >
+          <Tab value={currentTab} index={0} onClick={handleSidebarClick}>
+            <ListItemText>Theme Settings</ListItemText>
+          </Tab>
+          <Tab value={currentTab} index={1} onClick={handleSidebarClick}>
+            <ListItemText>Map Settings</ListItemText>
+          </Tab>
+        </List>
+      </Box>
+      <Box
+        sx={{
+          flex: 1,
+          p: 2,
+        }}
+      >
+        <TabPanel value={currentTab} index={0}>
+          <Typography variant="h5">Theme View</Typography>
+        </TabPanel>
+        <TabPanel value={currentTab} index={1}>
+          <Typography variant="h5">Map View</Typography>
+        </TabPanel>
+      </Box>
+    </Box>
+  );
 }
 
 export default Global;
