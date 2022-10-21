c45b3cd91953e950ddd7d04010443f54e1090777
Jafer Ahmed <op.jafer.ahmed@gmail.com>
2022-10-21 16:06:04 -0700

    fix: useLocation hook to select correct theme

--- src/hooks/globalHooks/useLocalStorage.js
+++ src/hooks/globalHooks/useLocalStorage.js
@@ -2,22 +2,23 @@ import { useState, useEffect } from 'react';
 
 // Get value from localStorage if possible, otherwise return provided default
 function getStorageValue(key, defaultValue) {
-  if (typeof window !== 'undefined') {
-    const value = localStorage.getItem(key);
-    let saved;
-    // log.warn('value:', value);
-    if (value && value !== 'undefined') {
-      saved = JSON.parse(value);
-    }
-    return saved || defaultValue;
+  const value = localStorage.getItem(key);
+  let saved;
+  // log.warn('value:', value);
+  if (value && value !== 'undefined') {
+    saved = JSON.parse(value);
   }
-  return defaultValue;
+  return saved || defaultValue;
 }
 
 // Usage: Similar to useState. Accepts a key, value pair. For example:
 // const [mode, setMode] = useLocalStorage('theme', 'light');
 const useLocalStorage = (key, defaultValue) => {
-  const [value, setValue] = useState(() => getStorageValue(key, defaultValue));
+  const [value, setValue] = useState(undefined);
+
+  useEffect(() => {
+    setValue(getStorageValue(key, defaultValue));
+  }, []);
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
