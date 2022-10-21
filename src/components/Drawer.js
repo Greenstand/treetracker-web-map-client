7a1729fecd431f842c315b2150d0ee33c87a02e5
khalatevarun <khalatevarun@gmail.com>
2022-10-21 19:17:59 +0530

    feat: add shadow to drawer title

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -280,8 +280,6 @@ export default function Drawer(props) {
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
-            boxShadow:
-              'rgb(0 0 0 / 20%) 0px 2px 4px -1px, rgb(0 0 0 / 14%) 0px 4px 5px 0px, rgb(0 0 0 / 12%) 0px 1px 10px 0px', // box shadow prop in parity with navbar shadow
           }}
         >
           <Box
@@ -297,6 +295,9 @@ export default function Drawer(props) {
             sx={{
               width: 1,
               pointerEvents: 'none',
+              boxShadow:
+                'rgb(0 0 0 / 20%) 0px 2px 4px -30px, rgb(0 0 0 / 14%) 0px 4px 5px 0px, rgb(0 0 0 / 12%) 0px 1px 10px -30px', // box shadow prop in parity with navbar shadow
+              zIndex: '100',
             }}
             id="drawer-title-container"
           />
