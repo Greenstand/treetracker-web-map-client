5e24e7acacb1fd96b2fc69ecf8e14417708d582c
David Ilas <davidilas1@gmail.com>
2022-11-06 19:31:02 +0100

    deleting Loto font after review

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,10 +13,6 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
             rel="stylesheet"
           />
