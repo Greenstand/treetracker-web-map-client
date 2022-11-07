04d428a0e1dbc2d58e604b6a8cf5f367dc6ff963
RubenSmn <31519867+RubenSmn@users.noreply.github.com>
2022-11-07 10:46:28 +0100

    fix: remove unnecessary links

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,11 +13,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Helvetica:wght@400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
             rel="stylesheet"
           />
         </Head>
