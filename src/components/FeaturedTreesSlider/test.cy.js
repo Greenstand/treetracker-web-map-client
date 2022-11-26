a074f06f7f13a5c8e4dc5e7e597c5d124998b6aa
Jafer Ahmed <op.jafer.ahmed@gmail.com>
2022-11-26 15:51:09 -0800

    fix: update image link to work with getThumbnailImageUrls

--- src/components/FeaturedTreesSlider/test.cy.js
+++ src/components/FeaturedTreesSlider/test.cy.js
@@ -1,48 +1,51 @@
 import FeaturedTreesSlider from './index';
 import { mountWithTheme as mount } from '../../models/test-utils';
 
+const treeImageLink =
+  'https://treetracker-production-images.s3.eu-central-1.amazonaws.com/2021.07.05.14.00.45_-3.2064076340000005_36.640772894_7b988c04-668e-48f5-b2e5-906f7d8374eb_IMG_20210705_132121_643781854.jpg';
+
 describe('Featured Trees Slider', () => {
   it('it shows featured trees', () => {
     // fake data
     const trees = [
       {
         id: '115059',
-        image_url: 'https://picsum.photos/id/10/208',
+        image_url: treeImageLink,
         species: 'Palm Tree',
       },
       {
         id: '115059',
-        image_url: 'https://picsum.photos/id/1037/208',
+        image_url: treeImageLink,
         species: 'Palm Tree',
       },
       {
         id: '115059',
-        image_url: 'https://picsum.photos/id/106/208',
+        image_url: treeImageLink,
         species: 'Palm Tree',
       },
       {
         id: '115059',
-        image_url: 'https://picsum.photos/id/107/208',
+        image_url: treeImageLink,
         species: 'Palm Tree',
       },
       {
         id: '115059',
-        image_url: 'https://picsum.photos/id/108/208',
+        image_url: treeImageLink,
         species: 'Palm Tree',
       },
       {
         id: '115059',
-        image_url: 'https://picsum.photos/id/109/208',
+        image_url: treeImageLink,
         species: 'Palm Tree',
       },
       {
         id: '115059',
-        image_url: 'https://picsum.photos/id/111/208',
+        image_url: treeImageLink,
         species: 'Palm Tree',
       },
       {
         id: '115059',
-        image_url: 'https://picsum.photos/id/112/208',
+        image_url: treeImageLink,
         species: 'Palm Tree',
       },
     ];
