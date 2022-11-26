d4253f991f639cd1c0b89a580735cc2029b08514
Jafer Ahmed <op.jafer.ahmed@gmail.com>
2022-11-26 15:41:43 -0800

    fix: hover on button

--- src/components/Home.js
+++ src/components/Home.js
@@ -150,6 +150,9 @@ export default function Home(props) {
               sx={{
                 color: '#474B4F',
                 ml: [4, 6],
+                '.MuiButton-root&:hover': {
+                  color: 'white',
+                },
               }}
             >
               Let&apos;s Find a Tree
