e603b9266f9a9282dea18e13135c673424ef78a3
keyy123 <parktwins1@gmail.com>
2022-11-30 21:19:02 -0500

    chore: run lint:fix script to pass failing unit test in github CI/CD

--- cypress/tests/units/admin/theme.cy.js
+++ cypress/tests/units/admin/theme.cy.js
@@ -1,5 +1,5 @@
-import ThemePlayground from '../../../../src/pages/admin/theme';
 import { mountWithTheme as mount } from '../../../../src/models/test-utils';
+import ThemePlayground from '../../../../src/pages/admin/theme';
 
 describe('Theme', () => {
   it('renders', () => {
