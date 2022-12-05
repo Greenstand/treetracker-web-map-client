74323ed7faba8a0d0e4a9c6e129d80f59d1e9197
Jafer Ahmed <op.jafer.ahmed@gmail.com>
2022-12-05 15:42:37 -0800

    fix: align cards on the wallets page

--- src/pages/wallets/[walletid].js
+++ src/pages/wallets/[walletid].js
@@ -305,6 +305,7 @@ export default function Wallet(props) {
                 title="Token ID"
                 icon={<Icon icon={TokenIcon} />}
                 link={`/wallets/${wallet.id}/tokens/${token.id}`}
+                fullWidth
               />
             </Box>
           ))}
