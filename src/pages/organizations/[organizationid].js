8f5b3dfee19ccc1ce56fe98bc415d9dc773973b7
Jafer Ahmed <op.jafer.ahmed@gmail.com>
2022-12-08 16:32:29 -0800

    fix: replace hardcoded continent value for CustomWorldMap

--- src/pages/organizations/[organizationid].js
+++ src/pages/organizations/[organizationid].js
@@ -368,7 +368,7 @@ export default function Organization(props) {
                     )) ||
                   undefined
                 }
-                con="af"
+                con={organization?.continent_name || 'af'}
               />
             </Box>
             <Typography
