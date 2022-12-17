01ebb04b0033f2f5b282e2265971a9227d493226
Jafer Ahmed <op.jafer.ahmed@gmail.com>
2022-12-16 16:27:48 -0800

    feat: add robot meta tag

--- src/components/HeadTag.js
+++ src/components/HeadTag.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 
-export default function HeadTag({ title }) {
+export default function HeadTag({ title, robotParameters }) {
   return (
     <Head>
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      {robotParameters && <meta name="robots" content={robotParameters} />}
       <title>
         {title
           ? `${title} | Treetracker by Greenstand`
