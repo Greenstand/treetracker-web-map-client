c8b64bdcb35becf628f4d9b85ff8e1992adbde86
Jafer Ahmed <op.jafer.ahmed@gmail.com>
2022-12-18 15:42:22 -0800

    fix: show placeholder image when user enters invalid link

--- src/components/dashboard/ChangeLogoSection.js
+++ src/components/dashboard/ChangeLogoSection.js
@@ -1,14 +1,19 @@
 import { Grid, Typography } from '@mui/material';
 import { useConfigContext } from 'context/configContext';
+import imagePlaceholder from 'images/image-placeholder.png';
 import CustomLogo from './CustomLogo';
 
 function ChangeLogoSection() {
   const { state } = useConfigContext();
 
+  const onInvalidImage = (e) => {
+    e.target.src = imagePlaceholder;
+  };
+
   return (
     <Grid item xs={6}>
       <Typography>Change the Logo</Typography>
-      <img src={state.navbar.logoUrl} height="300" />
+      <img src={state.navbar.logoUrl} height="300" onError={onInvalidImage} />
       <CustomLogo />
     </Grid>
   );
