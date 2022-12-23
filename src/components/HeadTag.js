07f36e5d85c38b5057b4ff6074f18dfd3275748c
Jafer Ahmed <op.jafer.ahmed@gmail.com>
2022-12-22 16:27:34 -0800

    feat: add more tags for open graph image

--- src/components/HeadTag.js
+++ src/components/HeadTag.js
@@ -1,28 +1,12 @@
-/* eslint-disable react/jsx-key */
+ 
 import Head from 'next/head';
 
-
-export default function HeadTag({ title, openGraph, robotParameters, twitter }) {
-  const openGraphTags = (
-    <>
-      {openGraph?.title && (
-        <meta property="og:title" content={openGraph.title} />
-      )}
-      {openGraph?.description && (
-        <meta property="og:description" content={openGraph.description} />
-      )}
-      {openGraph?.images &&
-        openGraph.images.map((image) => (
-          <meta property="og:image" content={image} />
-        ))}
-      {openGraph?.url && <meta property="og:url" content={openGraph.url} />}
-      {openGraph?.type && <meta property="og:type" content={openGraph.type} />}
-      {openGraph?.locale && (
-        <meta property="og:locale" content={openGraph.locale} />
-      )}
-    </>
-  );
-       
+export default function HeadTag({
+  title,
+  openGraph,
+  robotParameters,
+  twitter,
+}) {
   return (
     <Head>
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
@@ -32,7 +16,26 @@ export default function HeadTag({ title, openGraph, robotParameters, twitter }) 
           ? `${title} | Treetracker by Greenstand`
           : 'Treetracker by Greenstand'}
       </title>
-      {openGraphTags}
+      {openGraph?.title && (
+        <meta property="og:title" content={openGraph.title} />
+      )}
+      {openGraph?.description && (
+        <meta property="og:description" content={openGraph.description} />
+      )}
+      {openGraph?.images &&
+        openGraph.images.map((image) => (
+          <>
+            <meta property="og:image" content={image.url} />
+            <meta property="og:image:width" content={image.width} />
+            <meta property="og:image:height" content={image.height} />
+            <meta property="og:image:alt" content={image.alt} />
+          </>
+        ))}
+      {openGraph?.url && <meta property="og:url" content={openGraph.url} />}
+      {openGraph?.type && <meta property="og:type" content={openGraph.type} />}
+      {openGraph?.locale && (
+        <meta property="og:locale" content={openGraph.locale} />
+      )}
       {twitter?.title && <meta name="twitter:title" content={twitter.title} />}
       {twitter?.description && (
         <meta name="twitter:description" content={twitter.description} />
