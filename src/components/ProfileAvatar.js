9aeed48846a25e476fcc82e376973f6ea8a3816a
salinda Ekanayke <salindae@pop-os.localdomain>
2022-12-28 21:10:39 +0530

    fix: remove 'boxSizing' from 'noBackground'

--- src/components/ProfileAvatar.js
+++ src/components/ProfileAvatar.js
@@ -8,6 +8,7 @@ function ProfileAvatar({ src, rotation, sx = {}, noBackground = false }) {
         width: [120, 189],
         height: [120, 189],
         transform: rotation && `rotate(${rotation}deg)`,
+        boxSizing: 'border-box',
         ml: [4, 8],
         mt: [-98 / 4, -146 / 4],
         ...(noBackground
@@ -17,7 +18,6 @@ function ProfileAvatar({ src, rotation, sx = {}, noBackground = false }) {
               borderStyle: 'solid',
               borderColor: (t) => t.palette.background.paper,
               backgroundColor: (t) => t.palette.background.avatar,
-              boxSizing: 'border-box',
             }),
         ...sx,
       }}
