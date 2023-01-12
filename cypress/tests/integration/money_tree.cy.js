0464ba888911100f33971e24afc24334aa69a7a2
keyy123 <parktwins1@gmail.com>
2023-01-12 12:41:50 -0500

    chore: revised cypress documentation draft

--- cypress/tests/integration/money_tree.cy.js
+++ cypress/tests/integration/money_tree.cy.js
@@ -9,7 +9,7 @@ describe('Money Tree', () => {
   it('get a money tree', () => {
     const path = `/trees/${tree.id}`;
     prepareNocks({ tree: { ...tree, species_name: 'Money Tree' } });
-    // nocksIntercept for extra data that I want to show up
+    // challenge: Use nocksIntercept to add the correct additional api calls for trees/:id route
     cy.visit(path);
     // misc code to confirm changes
     cy.get('.mui-nmovop > :nth-child(1)').contains(`Money Tree`);
