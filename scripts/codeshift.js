d6f80eaa0bd481aa7b2509a05cacf7694fee0629
khalatevarun <khalatevarun@gmail.com>
2023-01-30 23:28:40 +0530

    feat: update codeshift file to handle all cases

--- scripts/codeshift.js
+++ scripts/codeshift.js
@@ -2,6 +2,7 @@
 // Read more: https://github.com/facebook/jscodeshift#parser
 export const parser = 'tsx';
 
+// Press ctrl+space for code completion
 export default function transformer(file, api) {
   const j = api.jscodeshift;
 
@@ -17,22 +18,39 @@ export default function transformer(file, api) {
     'mapContext',
   ];
 
+  // utility to remove all constructs('../') from the import statments
+  const getImportWithoutConstructs = (nodeValue) => {
+    let value = nodeValue;
+    while (value.includes('../')) {
+      value = value.replace('../', '');
+    }
+
+    return value;
+  };
+
   const normalImports = source
     .find(j.ImportDeclaration) // Find all nodes that match a type of `ImportDeclaration`
-    .filter((path) =>
-      importAliasArray.includes(path.node.source.value.split('/')[2]),
-    );
+    .filter((path) => {
+      const nodeValue = getImportWithoutConstructs(path.node.source.value);
 
-  normalImports.forEach((normalImport) =>
-    // Replace the existing node with a new one
+      return importAliasArray.includes(nodeValue.split('/')[0]);
+    }); // Filter imports by source equal to the target
 
-    j(normalImport).replaceWith(
-      // Build a new import declaration node based on the existing one
-      j.importDeclaration(
-        normalImport.node.specifiers,
-        j.stringLiteral(normalImport.node.source.value.slice(6)),
+  normalImports.forEach(
+    (
+      normalImport, // Iterate over react imports
+    ) =>
+      // Replace the existing node with a new one
+
+      j(normalImport).replaceWith(
+        // Build a new import declaration node based on the existing one
+        j.importDeclaration(
+          normalImport.node.specifiers, // copy over the existing import specificers
+          j.stringLiteral(
+            getImportWithoutConstructs(normalImport.node.source.value),
+          ), // Replace the source with our new source
+        ),
       ),
-    ),
   );
 
   return source.toSource({ quote: 'single' });
