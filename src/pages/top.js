a6962d56676b0cfa4905ff3f90801b03c0929075
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-03-18 12:28:03 -0400

    fix: migrate from v1 to  v2 api

--- src/pages/top.js
+++ src/pages/top.js
@@ -4,25 +4,24 @@ import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import log from 'loglevel';
 import React from 'react';
+import FeaturedPlantersSlider from 'components/FeaturedPlantersSlider';
+import FeaturedTreesSlider from 'components/FeaturedTreesSlider';
 import HeadTag from 'components/HeadTag';
+import LeaderBoard from 'components/LeaderBoard';
 import SearchButton from 'components/SearchButton';
-import { getCountryLeaderboard, getFeaturedTrees } from 'models/api';
-import FeaturedPlantersSlider from '../components/FeaturedPlantersSlider';
-import FeaturedTreesSlider from '../components/FeaturedTreesSlider';
-import LeaderBoard from '../components/LeaderBoard';
-// import SearchFilter from '../components/SearchFilter';
-import TagChips from '../components/TagChips';
-import Crumbs from '../components/common/Crumbs';
-import Icon from '../components/common/CustomIcon';
-import Filter from '../components/common/Filter';
-import { useFullscreen } from '../hooks/globalHooks';
-import Search from '../images/search.svg';
-import { useMapContext } from '../mapContext';
-import * as utils from '../models/utils';
+import TagChips from 'components/TagChips';
+import Crumbs from 'components/common/Crumbs';
+import Icon from 'components/common/CustomIcon';
+import Filter from 'components/common/Filter';
+import { useFullscreen } from 'hooks/globalHooks';
+import Search from 'images/search.svg';
+import { useMapContext } from 'mapContext';
+import { getCaptures, getCountryLeaderboard, getFeaturedGrowers, getFeaturedTrees } from 'models/api';
+import * as utils from 'models/utils';
 
 function Top(props) {
   log.warn('props top:', props);
-  const { trees, planters, countries, organizations, wallets } = props;
+  const { captures, growers, countries, organizations, wallets } = props;
   // use map context to get the map
   const { map } = useMapContext();
   const isFullscreen = useFullscreen();
@@ -83,181 +82,175 @@ function Top(props) {
     map.rerender();
   }
 
-  return (
-    <>
-      <HeadTag title="Tree Spotlight" />
-      <Box px={4} py={3} sx={{ maxWidth: '100%', boxSizing: 'border-box' }}>
-        {!isFullscreen && false && (
-          <Stack direction="row" justifyContent="flex-end" mb={6.125}>
-            <SearchButton />
-          </Stack>
-        )}
+  return (<>
+    <HeadTag title="Tree Spotlight" />
+    <Box px={4} py={3} sx={{ maxWidth: '100%', boxSizing: 'border-box' }}>
+      {!isFullscreen && false && (
+        <Stack direction="row" justifyContent="flex-end" mb={6.125}>
+          <SearchButton />
+        </Stack>
+      )}
 
-        {/* {!isFullscreen && <SearchFilter />} */}
+      {/* {!isFullscreen && <SearchFilter />} */}
 
-        <Box
-          sx={{
-            height: [4, 13],
-          }}
+      <Box
+        sx={{
+          height: [4, 13],
+        }}
+      />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Crumbs
+          items={[
+            {
+              // icon: <HomeIcon />,
+              name: 'Home',
+              url: '/',
+            },
+            {
+              name: 'tree spotlight',
+            },
+          ]}
         />
-        <Box
+        <Icon
+          icon={Search}
+          width={48}
+          height={48}
+          color="grey"
           sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
+            fill: 'transparent',
+            '& path': {
+              fill: 'grey',
+            },
           }}
-        >
-          <Crumbs
-            items={[
-              {
-                // icon: <HomeIcon />,
-                name: 'Home',
-                url: '/',
-              },
-              {
-                name: 'tree spotlight',
-              },
-            ]}
-          />
-          <Icon
-            icon={Search}
-            width={48}
-            height={48}
-            color="grey"
-            sx={{
-              fill: 'transparent',
-              '& path': {
-                fill: 'grey',
-              },
-            }}
-          />
-        </Box>
-        {trees?.length > 0 && (
-          <>
-            <Box
-              sx={{
-                mt: 8,
-              }}
-            >
-              <Typography variant="h4">Featured trees this week</Typography>
-            </Box>
-            {false && ( // going to be replaced by search filter component
-              <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <Filter onFilter={handleFilter} />
-              </Box>
-            )}
-            <Box>
-              <FeaturedTreesSlider trees={trees} isMobile={isFullscreen} />
-            </Box>
-          </>
-        )}
-        {organizations.length > 0 && (
-          <>
-            <Box sx={{ mt: [4, 8] }} />
-            <Typography variant="h4">
-              Featured organizations this week
-            </Typography>
-            <FeaturedPlantersSlider
-              link={(id) => `/organizations/${id}`}
-              color="primary"
-              planters={organizations}
-              isMobile={isFullscreen}
-            />
-          </>
-        )}
-        {planters.length > 0 && (
-          <>
-            <Box
-              sx={{
-                mt: 8,
-              }}
-            >
-              <Typography variant="h4">Featured planters this week</Typography>
-            </Box>
-            <FeaturedPlantersSlider
-              link={(id) => `/planters/${id}`}
-              color="secondary"
-              planters={planters}
-              isMobile={isFullscreen}
-            />
-          </>
-        )}
-        {wallets.length > 0 && (
-          <>
-            <Typography variant="h4">Featured wallets this week</Typography>
-            <FeaturedPlantersSlider
-              link={(id) => `/wallets/${id}`}
-              color="secondary"
-              planters={wallets}
-              isMobile={isFullscreen}
-            />
-          </>
-        )}
-        <Typography
-          variant="h4"
-          sx={{
-            marginTop: 8,
-          }}
-        >
-          Check out the global leaders in the tree planting effort
-        </Typography>
-        <Box
-          sx={{
-            padding: (t) => [t.spacing(4, 0, 0, 0), t.spacing(8, 0, 0, 0)],
-          }}
-        >
-          <TagChips
-            tagItems={continentTags}
-            onSelectTag={(continent) => {
-              setContinentTag(continent);
-            }}
-          />
-        </Box>
-        <Box sx={{ marginTop: 18 }} />
-        <LeaderBoard
-          countries={leaderboardCountries}
-          handleCountryClick={handleCountryClick}
         />
       </Box>
-      {isFullscreen && (
-        <Portal container={document.getElementById('drawer-title-container')}>
+      {captures?.length > 0 && (
+        <>
           <Box
             sx={{
-              px: 4,
-              pb: 4,
+              mt: 8,
             }}
           >
-            <Typography variant="h2" color="primary">
-              Treetracker Spotlight
-            </Typography>
+            <Typography variant="h4">Featured trees this week</Typography>
           </Box>
-        </Portal>
-      )}
-      {isFullscreen && (
-        <Portal
-          container={document.getElementById('drawer-title-container-min')}
-        >
+          {false && ( // going to be replaced by search filter component
+            (<Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Filter onFilter={handleFilter} />
+            </Box>)
+          )}
           <Box>
-            <Typography variant="h4">Treetracker Spotlight</Typography>
+            <FeaturedTreesSlider trees={captures} isMobile={isFullscreen} />
           </Box>
-        </Portal>
+        </>
       )}
-    </>
-  );
+      {organizations.length > 0 && (
+        <>
+          <Box sx={{ mt: [4, 8] }} />
+          <Typography variant="h4">
+            Featured organizations this week
+          </Typography>
+          <FeaturedPlantersSlider
+            link={(id) => `/organizations/${id}`}
+            color="primary"
+            planters={organizations}
+            isMobile={isFullscreen}
+          />
+        </>
+      )}
+      {growers.length > 0 && (
+        <>
+          <Box
+            sx={{
+              mt: 8,
+            }}
+          >
+            <Typography variant="h4">Featured planters this week</Typography>
+          </Box>
+          <FeaturedPlantersSlider
+            link={(id) => `/planters/${id}`}
+            color="secondary"
+            planters={growers}
+            isMobile={isFullscreen}
+          />
+        </>
+      )}
+      {wallets.length > 0 && (
+        <>
+          <Typography variant="h4">Featured wallets this week</Typography>
+          <FeaturedPlantersSlider
+            link={(id) => `/wallets/${id}`}
+            color="secondary"
+            planters={wallets}
+            isMobile={isFullscreen}
+          />
+        </>
+      )}
+      <Typography
+        variant="h4"
+        sx={{
+          marginTop: 8,
+        }}
+      >
+        Check out the global leaders in the tree planting effort
+      </Typography>
+      <Box
+        sx={{
+          padding: (t) => [t.spacing(4, 0, 0, 0), t.spacing(8, 0, 0, 0)],
+        }}
+      >
+        <TagChips
+          tagItems={continentTags}
+          onSelectTag={(continent) => {
+            setContinentTag(continent);
+          }}
+        />
+      </Box>
+      <Box sx={{ marginTop: 18 }} />
+      <LeaderBoard
+        countries={leaderboardCountries}
+        handleCountryClick={handleCountryClick}
+      />
+    </Box>
+    {isFullscreen && (
+      <Portal container={document.getElementById('drawer-title-container')}>
+        <Box
+          sx={{
+            px: 4,
+            pb: 4,
+          }}
+        >
+          <Typography variant="h2" color="primary">
+            Treetracker Spotlight
+          </Typography>
+        </Box>
+      </Portal>
+    )}
+    {isFullscreen && (
+      <Portal
+        container={document.getElementById('drawer-title-container-min')}
+      >
+        <Box>
+          <Typography variant="h4">Treetracker Spotlight</Typography>
+        </Box>
+      </Portal>
+    )}
+  </>);
 }
 
 async function serverSideData(params) {
-  const [trees, countries, planters, organizations, wallets] =
+  const [captures, countries, growers, organizations, wallets] =
     await Promise.all([
-      getFeaturedTrees(), //
+      getCaptures(), //
       process.env.NEXT_PUBLIC_COUNTRY_LEADER_BOARD_DISABLED === 'true'
         ? []
         : getCountryLeaderboard(),
-      (async () => {
-        const data = await utils.requestAPI('/planters/featured');
-        log.warn('planters', data);
-        return data.planters;
-      })(),
+      getFeaturedGrowers(),
       (async () => {
         const data = await utils.requestAPI('/organizations/featured');
         log.warn('organizations', data);
@@ -270,9 +263,9 @@ async function serverSideData(params) {
       })(),
     ]);
   return {
-    trees,
+    captures,
     countries,
-    planters,
+    growers,
     organizations,
     wallets,
   };
