9d31c2774a263d415624358182435f91bc1c1e69
Faisal Farooq <74142521+DevFaisal@users.noreply.github.com>
2023-05-20 09:32:00 +0530

    Update LikeButton.js
    Go through this may be it will work in this way

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,41 +1,31 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 function LikeButton({ url }) {
-  const loadScript = () =>
-    new Promise((resolve, reject) => {
-      const loaded = document.getElementById('fbsdk');
-      if (loaded) resolve();
-
-      const script = document.createElement('script');
-      script.id = 'fbsdk';
-      script.setAttribute('crossOrigin', 'anonymous');
-      script.src =
-        'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0';
-      script.setAttribute('nonce', 'B4dTchZ8');
-      script.type = 'text/javascript';
-      script.onload = resolve;
-      script.onerror = reject;
-      document.body.append(script);
-    });
-
   useEffect(() => {
-    loadScript().then(() => {
-      if (typeof window.FB !== 'undefined') {
-        window.FB.XFBML.parse();
-      }
-    });
+    // Load the Facebook JavaScript SDK asynchronously
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId: 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0'
+        xfbml: true,
+        version: 'v12.0'
+      });
+      FB.XFBML.parse(); // Render the Like button
+    };
+
+    // Insert the Facebook SDK script dynamically
+    const script = document.createElement('script');
+    script.src = 'https://connect.facebook.net/en_US/sdk.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Cleanup function to remove the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
   }, []);
 
   return (
-    <div
-      className="fb-like"
-      data-href={url}
-      data-width=""
-      data-layout="box_count"
-      data-action="like"
-      data-size="small"
-      data-share="false"
-    />
+    <div className="fb-like" data-href={url} data-width="" data-layout="box_count" data-action="like" data-size="small" data-share="false" />
   );
 }
 
