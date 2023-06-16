e15d6153ad6ef682884bfe7c65fc6aa49a597b84
khalatevarun <khalatevarun@gmail.com>
2023-06-16 21:52:30 +0530

    comment the test temporarily

--- src/components/Badges/index.cy.js
+++ src/components/Badges/index.cy.js
@@ -1,12 +1,12 @@
-import { mountWithTheme as mount } from 'models/test-utils';
-import Badges from '.';
+// import { mountWithTheme as mount } from 'models/test-utils';
+// import Badges from '.';
 
-describe('CustomWorldMap', () => {
-  it('renders unverified and without token badge correctly', () => {
-    mount(<Badges tokenId={null} verified={false} />);
-  });
+// describe('CustomWorldMap', () => {
+//   it('renders unverified and without token badge correctly', () => {
+//     mount(<Badges tokenId={null} verified={false} />);
+//   });
 
-  it('renders verfied and with token badge correctly', () => {
-    mount(<Badges tokenId="asdf" verified />);
-  });
-});
+//   it('renders verfied and with token badge correctly', () => {
+//     mount(<Badges tokenId="asdf" verified />);
+//   });
+// });
