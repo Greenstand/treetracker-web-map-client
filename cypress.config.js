5c3ee5bc6e1182e410f2cba496395f4a85a9b29b
deanchen <antlet2004@gmail.com>
2023-06-27 11:21:18 +0800

    fix: broken int test in cypress

--- cypress.config.js
+++ cypress.config.js
@@ -9,10 +9,6 @@ module.exports = defineConfig({
     watch: ['src/**'],
   },
 
-  env: {
-    nock: false,
-  },
-
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
