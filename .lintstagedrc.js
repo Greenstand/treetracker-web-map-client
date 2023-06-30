8b8f40077d7c825651e67f9ffcf518a5afa64e21
deanchen <antlet2004@gmail.com>
2023-06-30 10:11:05 +0800

    feat: prettier after lint fix

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,11 +5,8 @@ module.exports = {
   // import alias for uniform pattern of imports
   'src/**/*.js': ['jscodeshift  -t scripts/codeshift.js'],
 
-  // format all file types recognized by prettier
-  '*': ['prettier --ignore-unknown --write'],
-
   // lint javascript after formatting
-  '*.{js,jsx}': ['eslint --fix --cache'],
+  '*.{js,jsx}': ['eslint --fix --cache', 'prettier --ignore-unknown --write'],
 
   // lint entire project if eslint settings changed, do not pass file name arguments
   '.eslint*': () => 'eslint . --cache',
