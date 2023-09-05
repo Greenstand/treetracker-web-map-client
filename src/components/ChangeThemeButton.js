d02cc3e6d25a6c3b8eda3e6cde6264fea2785f81
Jafer Ahmed <op.jafer.ahmed@gmail.com>
2023-09-04 21:01:19 -0700

    fix: update theme button alt tag and announce to screenreader

--- src/components/ChangeThemeButton.js
+++ src/components/ChangeThemeButton.js
@@ -1,13 +1,22 @@
 import DarkMode from '@mui/icons-material/DarkMode';
 import LightMode from '@mui/icons-material/LightMode';
 import IconButton from '@mui/material/IconButton';
-import { useCustomThemeContext } from '../hooks/contextHooks';
+import { useCustomThemeContext } from 'hooks/contextHooks';
 
 export default function ChangeThemeButton() {
   const { colorMode, theme } = useCustomThemeContext();
 
+  const alt =
+    theme.palette.mode === 'light'
+      ? 'Turn on dark mode (currently light mode)'
+      : 'Turn on light mode (currently dark mode)';
+
   return (
-    <IconButton aria-label="change theme" onClick={colorMode.toggleColorMode}>
+    <IconButton
+      aria-label={alt}
+      onClick={colorMode.toggleColorMode}
+      aria-live="polite"
+    >
       {theme.palette.mode === 'light' ? <DarkMode /> : <LightMode />}
     </IconButton>
   );
