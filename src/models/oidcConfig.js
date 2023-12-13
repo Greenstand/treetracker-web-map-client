84e4d1cc95ef53f48351d4aebbb90d77751ee362
dadiorchen <antlet2004@gmail.com>
2023-12-13 11:20:24 +0800

    fix: use env for keycloak

--- src/models/oidcConfig.js
+++ src/models/oidcConfig.js
@@ -1,8 +1,8 @@
 const oidcConfig = {
-  authority: 'https://dev-k8s.treetracker.org/keycloak/realms/treetracker/',
-  client_id: 'raw-client',
-  redirect_uri: 'http://localhost:3000/',
-  realm: 'treetracker',
+  authority: process.env.NEXT_PUBLIC_KEYCLOAK_URL,
+  client_id: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID,
+  redirect_uri: process.env.NEXT_PUBLIC_KEYCLOAK_REDIRECT_URI,
+  realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM,
   onSigninCallback: (res) => {
     console.log('onSigninCallback', res);
     localStorage.setItem('res', JSON.stringify(res));
